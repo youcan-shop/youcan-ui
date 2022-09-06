144c0ecd8ab7ca4d5aec70a209182628b54920ee
AliHMIMS <85164582+AliHMIMS@users.noreply.github.com>
2022-09-06 17:00:39 +0100

    Basic groupSelect

---
+++ src/components/YGroupSelect/YGroupSelect.story.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import YGroupSelect from './YGroupSelect.vue';
+</script>
+
+<template>
+  <Story>
+    <YGroupSelect />
+  </Story>
+</template>
---
+++ src/components/YGroupSelect/YGroupSelect.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import YButton from '~/components/YButton/YButton.vue';
+
+// defineProps<{}>();
+
+const selectedOption = ref(null);
+</script>
+
+<template>
+  <div>
+    <YButton icon-position="right">
+      Please select an option
+      <template #icon>
+        <i i-tabler-selector />
+      </template>
+    </YButton>
+  </div>
+</template>
+
+<style scoped></style>
