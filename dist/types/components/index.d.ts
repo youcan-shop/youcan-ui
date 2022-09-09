bbe2873557b15a4bea2a40432ea46de84cdd4548
Aymane D. Hlamnach <aymane.hlamnach@youcan.shop>
2022-09-09 09:46:26 +0100

    Dark:  Fix component exports

--- dist/types/components/index.d.ts
+++ dist/types/components/index.d.ts
@@ -1,5 +1,5 @@
-export * as YInput from '~/components/YInput/YInput.vue';
-export * as YButton from '~/components/YButton/YButton.vue';
-export * as YGroupSelect from '~/components/YGroupSelect/YGroupSelect.vue';
-export * as YGroupSelectMenu from '~/components/YGroupSelect/YGroupSelectMenu.vue';
-export * as YSelect from '~/components/YSelect/YSelect.vue';
+export { default as YInput } from '~/components/YInput/YInput.vue';
+export { default as YButton } from '~/components/YButton/YButton.vue';
+export { default as YSelect } from '~/components/YSelect/YSelect.vue';
+export { default as YGroupSelect } from '~/components/YGroupSelect/YGroupSelect.vue';
+export { default as YGroupSelectMenu } from '~/components/YGroupSelect/YGroupSelectMenu.vue';
