077ff2d1e19b2c172c8447d13a172e172298f8ef
adildev101 <adil@youcan.shop>
2022-11-07 14:29:23 +0100

    remove the old components

--- src/_dev/App.vue
+++ src/_dev/App.vue
@@ -1,44 +1,10 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-import YInput from '~/components/Yinput/Yinput.vue';
-import YButton from '~/components/YButton/YButton.vue';
-import YGroupSelect from '~/components/YGroupSelect/YGroupSelect.vue';
-import YSelectOption from '~/components/YSelect/YSelectOption.vue';
 import '~/assets/main.scss';
 
 const inputString = ref('');
 </script>
 
 <template>
-  <YInput v-model="inputString" input-type="text" :has-help="true" hint="Subtle input hint" placeholder="Placeholder text" :is-error="false" label="Label" input-icon="i-tabler-home" />
-  <p>Input: {{ inputString }}</p>
-  <br>
-  <YButton icon-position="right">
-    <span>Button</span>
-    <template #icon>
-      <i i-tabler-apple />
-    </template>
-  </YButton>
-  <br>
-  <YGroupSelect
-    :selected-item="{
-      label: 'painer bread',
-      value: '3',
-    }"
-    :items="[
-      {
-        label: 'pune',
-        value: '1',
-      },
-      {
-        label: 'painer bread',
-        value: '3',
-      },
-      {
-        icon: 'i-tabler-home',
-        label: 'Select option',
-        value: '2',
-      },
-    ]"
-  />
+   
 </template>
