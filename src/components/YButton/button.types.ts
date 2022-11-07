8f324d7c81d8c0dd868abdc3a657adbed02acf25
adildev101 <adil@youcan.shop>
2022-11-07 12:18:05 +0100

    downgrade vuejs version to 2.7v

--- src/components/YButton/button.types.ts
+++
@@ -1,7 +0,0 @@
-export type size = 'sm' | 'md' | 'lg';
-
-export type hierarchy = 'primary' | 'secondary' | 'tertiary';
-
-export type color = 'brand' | 'gray' | 'red';
-
-export type iconPosition = 'left' | 'right';
