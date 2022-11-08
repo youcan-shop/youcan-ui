80773f38c9381c8b45bf2f03ebc6d112a24bf6fd
adildev101 <adil@youcan.shop>
2022-11-08 16:23:10 +0100

    add input text

---
+++ src/components/TextInput/TextInput.story.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import Input from '~/components/Input/Input.vue';
+const firstValue = ref('hello v1');
+</script>
+
+<template>
+  <Story>
+    <Variant title="Default">
+      <Input v-model="firstValue" placeholder="text" />
+      <span>Value: {{ firstValue }}</span>
+    </Variant>
+  </Story>
+</template>
---
+++ src/components/TextInput/TextInput.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import Input from '~/components/Input/Input.vue';
+
+const props = defineProps<{
+  value: string
+  placeholder?: string
+}>();
+
+const emit = defineEmits(['input']);
+const inputValue = ref(props.value);
+
+const onInput = (e: Event) => {
+  const target = e.target as HTMLInputElement;
+  inputValue.value = target.value;
+
+  emit('input', inputValue.value);
+};
+</script>
+
+<template>
+  <Input :value="inputValue" type="text" class="input" :placeholder="placeholder" @input="onInput" />
+</template>
