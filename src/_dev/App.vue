d2b2112473622b112086a7980c9e921c9ea91a4f
Anas <93322743+bj-anas@users.noreply.github.com>
2022-11-08 14:14:25 +0100

    DOT-6744 : UI Components > create button component (#17)
    * init button
    
    * create button component
    
    * request change

--- src/_dev/App.vue
+++ src/_dev/App.vue
@@ -1,14 +1,22 @@
 <script setup lang="ts">
+import '~/assets/main.scss';
 import { ref } from 'vue';
 import Input from '~/components/Input/Input.vue';
-import '~/assets/main.scss';
+import Button from '~/components/Button/Button.vue';
 
 const inputValue = ref('hello');
 </script>
 
 <template>
   <div>
-    <Input v-model="inputValue" type="text" placeholder="Placeholder" />
-    <span>{{ inputValue }}</span>
+    <div>
+      <Input v-model="inputValue" type="text" placeholder="Placeholder" />
+      <span>{{ inputValue }}</span>
+    </div>
+    <div>
+      <Button>
+        <span>Continue</span>
+      </Button>
+    </div>
   </div>
 </template>
