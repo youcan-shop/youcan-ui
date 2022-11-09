9ed6551b60db1657efeaefabc93b939502047cc2
adildev101 <adil@youcan.shop>
2022-11-09 09:44:56 +0100

    update the slug input

--- src/components/StoreSlugInput/StoreSlugInput.vue
+++ src/components/StoreSlugInput/StoreSlugInput.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { onMounted, ref, watch, watchEffect } from 'vue';
 import Input from '~/components/Input/Input.vue';
-
 const props = defineProps({
   value: {
     type: String,
@@ -24,28 +23,154 @@ const inputValue = ref(props.value);
 const onInput = (emittedValue: string) => {
   emit('input', emittedValue);
 };
+const focusMenu = ref(false);
+const onfocus = () => focusMenu.value = true;
+const onblur = () => focusMenu.value = false;
 </script>
 
 <template>
-  <div class="store-slug-input">
-    <Input :value="inputValue" type="text" class="input" :placeholder="placeholder" @input="onInput" />
-    <span class="slug-suffix">{{ slugSuffix }}</span>
+  <div class="container">
+    <div class="store-slug-input">
+      <Input
+        ref="onFocus" :value="inputValue"
+        type="text" class="input" :placeholder="placeholder" @input="onInput"
+        @focus="onfocus" @blur="onblur"
+      />
+      <span class="slug-suffix">{{ slugSuffix }}</span>
+    </div>
+    <Transition name="fade">
+      <div v-if="focusMenu" class="store-slug-box">
+        <div class="store-slug-box-content">
+          <p class="store-slug-title">
+            Store slug
+          </p>
+          <p class="store-slug-description">
+            The slug you choose will be your store's domain name that your customers will see.
+          </p>
+          <div class="border-example">
+            <i class="i-tabler-search" />
+            <span>example.youcan.shop</span>
+          </div>
+          <div class="note">
+            <div class="note-icon">
+              <i class="i-tabler-alert-circle" />
+            </div>
+            <span class="note-description">
+              You can always replace it with a custom domain name you purchased.
+            </span>
+          </div>
+        </div>
+      </div>
+    </Transition>
   </div>
 </template>
 
 <style scoped>
+.container {
+  display: flex;
+  align-items: flex-start;
+  gap: 10px;
+  position: relative;
+}
 .store-slug-input {
+  position: relative;
   display: flex;
   align-items: center;
+  width: 65%;
 }
 
 .store-slug-input span {
   position: absolute;
-  right: 16px;
-  color: var(--placeholder-color);
+  right: 20px;
+  color: var(--store-slug-input);
   font-weight: 400;
-  font-size: 16px;
+  font-size: 15px;
   line-height: 19px;
   letter-spacing: 0.04em;
+}
+
+.store-slug-box {
+  width: 332px  ;
+  border: 1px solid var(--store-slug-border-box);
+  box-shadow: 0px 4px 12px var(--store-slug-box-shadow);
+  border-radius: 12px;
+  position: absolute;
+  right: 0;
+}
+
+.store-slug-box-content {
+  padding-left: 20px;
+  padding-right: 28px;
+  margin: auto;
+}
+
+.store-slug-title {
+  font-weight: 500;
+  color: var(--store-slug-title);
+  font-size: 16px;
+  line-height: 19px;
+  letter-spacing: 0.02em;
+}
+
+.store-slug-description {
+  font-weight: 400;
+  color: var(--store-slug-description);
+  font-weight: 400;
+  font-size: 13px;
+  margin-top: -5px;
+  line-height: 20px;
+  letter-spacing: 0.02em;
+}
+
+.border-example {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  border: 1px solid var(--store-slug-box-input);
+  padding: 12px 16px;
+  box-shadow: 0px 1px 8px var(--store-slug-box-shadow);
+  border-radius: 8px;
+  margin-bottom: 14px;
+}
+
+.border-example i {
+  color: var(--store-slug-box-icon);
+}
+
+.border-example span {
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 19px;
+  letter-spacing: 0.02em;
+  color: var(--store-slug-description);
+}
+
+.note {
+  display: flex;
+  align-items: center;
+  gap: 9px;
+  margin-bottom: 12px;
+}
+
+.note-icon i {
+  color: var(--store-slug-note);
+}
+
+.note-description {
+  font-weight: 400;
+  font-size: 13px;
+  line-height: 21px;
+  letter-spacing: 0.02em;
+  color: var(--store-slug-note);
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
