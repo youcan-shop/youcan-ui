364922e77de39d7d128a96dc0aef8f4cf049a7f3
Aymane D. Hlamnach <aymane.hlamnach@youcan.shop>
2023-01-02 15:36:43 +0100

    wip

---
+++ tooling/transform.mjs
@@ -0,0 +1,122 @@
+import { truncateSync, writeFileSync } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import input from './tokens.json' assert { type: 'json' };
+
+const OUTPUT_PATH = path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'tokens.css');
+
+const FONT_WEIGHTS = {
+  'Thin': 100,
+  'Extra Light': 200,
+  'Light': 300,
+  'Regular': 400,
+  'Medium': 500,
+  'Semi Bold': 600,
+  'Bold': 700,
+  'Black': 800,
+};
+
+const TRANSFORMERS = {
+  color: (prefix, { value }) => {
+    return `--${prefix}: ${value.toUpperCase()};`;
+  },
+  boxShadow: (prefix, { value }) => {
+    const spread = v => `${v.x}px ${v.y}px ${v.blur}px ${v.spread}px ${v.color}`;
+
+    if (Array.isArray(value)) {
+      return `--${prefix}: ${value.map(spread).join(', ')};`;
+    }
+
+    return `--${prefix}: ${spread(value)};`;
+  },
+  typography: (prefix, { value }, set) => {
+    const {
+      fontWeight,
+      fontSize,
+      lineHeight,
+      fontFamily,
+    } = substituteValue(set, value);
+
+    const ratio = lineHeight === 'AUTO'
+      ? `${fontSize}px`
+      : `${fontSize}px/${lineHeight}`;
+
+    return `--${prefix}: ${FONT_WEIGHTS[fontWeight]} ${ratio} "${fontFamily}";`;
+  },
+};
+
+function isUnit(token) {
+  return typeof token === 'object' && 'value' in token && token.value != null;
+}
+
+function substituteValue(set, value) {
+  return Object
+    .fromEntries(
+      Object
+        .entries(value)
+        .map(([k, v]) => {
+          const matches = v.match(/{+(.*?)}/);
+
+          if (!matches) {
+            return [k, v];
+          }
+
+          try {
+            const substitution = matches[1]
+              .split('.')
+              .reduce((x, y) => x[y], set);
+
+            return [k, substitution.value];
+          }
+          catch (err) {
+            return [k, v];
+          }
+        }),
+    );
+}
+
+function prefix(path) {
+  return path
+    .join('-')
+    .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
+    .map(p => p.toLowerCase())
+    .join('-');
+}
+
+function transform(path, unit, set) {
+  const transformer = TRANSFORMERS[unit.type];
+  if (transformer == null) {
+    return null;
+  }
+
+  return transformer(prefix(path), unit, set);
+}
+
+let output = ':root {';
+
+function write(line) {
+  if (line) {
+    return output += `\n${line}`;
+  }
+}
+
+Object.values(input).forEach((set) => {
+  Object.entries(set).forEach(([tokenId, token]) => {
+    if (isUnit(token)) {
+      write(transform([tokenId], token, set));
+    }
+    else {
+      Object
+        .entries(token)
+        .forEach(([unitId, unit]) => {
+          write(transform([tokenId, unitId], unit, set));
+        });
+    }
+  });
+});
+
+output += '\n}';
+
+truncateSync(OUTPUT_PATH);
+writeFileSync(OUTPUT_PATH, output, { flag: 'w', encoding: 'utf-8' });
+
