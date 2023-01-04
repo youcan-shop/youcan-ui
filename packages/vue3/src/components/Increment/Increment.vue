22ad520ec7a30ac61a813d7601c5555f93cad994
Ali HMIMS <85164582+AliHMIMS@users.noreply.github.com>
2023-01-04 09:44:49 +0100

    ditch zag + manual increment logic

--- packages/vue3/src/components/Increment/Increment.vue
+++ packages/vue3/src/components/Increment/Increment.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
 import { Utils } from '@youcan/ui-core';
-import * as numberInput from '@zag-js/number-input';
-import { normalizeProps, useMachine } from '@zag-js/vue';
-import { computed, watchEffect } from 'vue';
+import { computed, onMounted, ref, watchEffect } from 'vue';
 import type { IncrementMachineValue } from './types';
 
 const props = withDefaults(defineProps<{
@@ -22,33 +20,78 @@ const emit = defineEmits(['update:modelValue']);
 
 const model = computed({
   get: () => props.modelValue,
-  set: (value: string) => emit('update:modelValue', value),
+  set: (value: string) => {
+    const inputValue = Number(value);
+
+    if (typeof props.max !== 'undefined' && inputValue > props.max) {
+      model.value = String(props.max);
+
+      return;
+    }
+    else if (typeof props.min !== 'undefined' && inputValue < props.min) {
+      model.value = String(props.min);
+
+      return;
+    }
+
+    emit('update:modelValue', value);
+  },
 });
 
-console.log(model.value);
+const input = ref<HTMLInputElement>();
 
-const [state, send] = useMachine(numberInput.machine({
-  value: '10',
-  // onChange: (value: IncrementMachineValue) => console.log(value),
-  id: props.id,
-}));
+const increment = () => {
+  model.value = String(Number(model.value) + props.step);
+};
 
-const api = computed(() =>
-  numberInput.connect(state.value, send, normalizeProps),
-);
+const decrement = () => {
+  model.value = String(Number(model.value) - props.step);
+};
 
-console.log(api.value.inputProps);
+onMounted(() => {
+  input.value?.addEventListener('beforeinput', (event) => {
+    if (event.data === '-') {
+      return;
+    }
+
+    const inputValue = Number(event.data);
+
+    if (isNaN(inputValue) || event.data === ' ') {
+      event.preventDefault();
+    }
+
+    if (
+      (typeof props.max !== 'undefined' && inputValue > props.max)
+      || (typeof props.min !== 'undefined' && inputValue < props.min)
+    ) {
+      event.preventDefault();
+    }
+  });
+
+  input.value?.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowUp') {
+      event.preventDefault();
+
+      increment();
+    }
+
+    if (event.key === 'ArrowDown') {
+      event.preventDefault();
+
+      decrement();
+    }
+  });
+});
 </script>
 
 <template>
-  <div ref="ref" v-bind="api.rootProps">
-    <label v-bind="api.labelProps">Enter number</label>
+  <div ref="ref">
     <div>
-      <button v-bind="api.decrementTriggerProps">
+      <button @click="decrement">
         DEC
       </button>
-      <input v-bind="api.inputProps" value="10">
-      <button v-bind="api.incrementTriggerProps">
+      <input :id="id" ref="input" v-model="model">
+      <button @click="increment">
         INC
       </button>
     </div>
