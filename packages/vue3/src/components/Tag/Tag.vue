28abf5f67738c5c5dac080593bff7a18dcdff968
Ali HMIMS <85164582+AliHMIMS@users.noreply.github.com>
2023-01-04 15:17:04 +0100

    Click container to focus input

--- packages/vue3/src/components/Tag/Tag.vue
+++ packages/vue3/src/components/Tag/Tag.vue
@@ -8,13 +8,19 @@ const props = withDefaults(defineProps<{
   modelValue: TagItemValue[]
   type?: TagItemType
   placeholder?: string
+  disabled?: boolean
+  error?: boolean
 }>(), {
   type: 'text',
+  placeholder: 'Add tag',
+  disabled: false,
+  error: false,
 });
 
 const emit = defineEmits(['update:modelValue']);
 
 const tagInput = ref<HTMLInputElement>();
+const tagsContainer = ref<HTMLDivElement>();
 const newTagLabel = ref('');
 
 const model = computed({
@@ -23,6 +29,10 @@ const model = computed({
 });
 
 const removeTag = (index: number) => {
+  if (props.disabled) {
+    return;
+  }
+
   model.value = Utils.removeFromArray(model.value, index);
 };
 
@@ -43,14 +53,20 @@ onMounted(() => {
       newTagLabel.value = '';
     }
   });
+
+  tagsContainer.value?.addEventListener('click', (event: MouseEvent) => {
+    if (event.target === tagsContainer.value) {
+      tagInput.value?.focus();
+    }
+  });
 });
 </script>
 
 <template>
-  <div class="tag">
+  <div ref="tagsContainer" class="tag" tabindex="0" :disabled="disabled" :error="error">
     <TagItem v-for="(tag, index) in model" :key="tag.id" v-model="model[index]" :type="type"
       @remove="removeTag(index)" />
-    <input ref="tagInput" v-model="newTagLabel" type="text" class="tag-input">
+    <input ref="tagInput" v-model="newTagLabel" type="text" class="tag-input" :placeholder="placeholder">
   </div>
 </template>
 
@@ -73,8 +89,47 @@ onMounted(() => {
   box-shadow: var(--shadow);
 }
 
+.tag:hover {
+  --border: 1px solid var(--gray-200);
+}
+
+.tag[disabled='false'][error='false']:focus,
+.tag[disabled='false'][error='false']:focus-within {
+  --border: 1px solid var(--brand-500);
+  --shadow: var(--focus-xs-brand);
+}
+
+.tag[disabled='false'][error='true'] {
+  --border: 1px solid var(--red-500);
+}
+
+.tag[disabled='false'][error='true']:focus,
+.tag[disabled='false'][error='true']:focus-within {
+  --border: 1px solid var(--red-500);
+  --shadow: var(--focus-xs-red);
+}
+
+.tag[disabled='true'] {
+  --text-color: var(--gray-300);
+  --background-color: var(--gray-50);
+}
+
+.tag[disabled='true'] .tag-input {
+  background-color: var(--gray-50);
+  pointer-events: none;
+}
+
 .tag .tag-input {
   flex: 1;
   min-width: 146px;
+  background-color: var(--base-white);
+  color: var(--gray-900);
+  font: var(--text-sm-regular);
+  border: none;
+  outline: none;
+}
+
+.tag .tag-input::placeholder {
+  color: var(--gray-300);
 }
 </style>
