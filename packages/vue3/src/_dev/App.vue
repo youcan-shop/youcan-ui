3826567a0dfa69280f5d223452cc54654899e747
Aymane D. Hlamnach <aymane.hlamnach@youcan.shop>
2023-01-04 16:56:06 +0100

    cleanup

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -1,43 +1,10 @@
 <script setup lang="ts">
-import { ref } from 'vue';
 import '~/assets/main.css';
-import {
-  FileInput,
-  UploadedFile,
-  UploadedMedia,
-} from '~/components';
-
-const fileArr = ref<File[]>([]);
-
-function handleinput(files: File[]) {
-  files.forEach(f => fileArr.value.push(f));
-}
-
-function deletefile(file: File) {
-  // naive deletion
-  fileArr.value = fileArr.value.filter(f => f.name !== file.name);
-}
 </script>
 
 <template>
   <div>
-    <FileInput size="md" @input="handleinput">
-      <template #label>
-        Upload :)
-      </template>
-    </FileInput>
-    <div class="files">
-      <UploadedFile v-for="file in fileArr" :key="file.name" :file="file" :progress="50" @delete="deletefile(file)" />
-    </div>
-    <div class="files">
-      <UploadedFile v-for="file in fileArr" :key="file.name" :file="file" error=":(" @delete="deletefile(file)" />
-    </div>
-    <div class="files">
-      <UploadedFile v-for="file in fileArr" :key="file.name" :file="file" @delete="deletefile(file)" />
-    </div>
-    <div class="files">
-      <UploadedMedia v-for="file in fileArr" :key="file.name" :file="file" @delete="deletefile(file)" />
-    </div>
+    a
   </div>
 </template>
 
--- packages/vue3/src/components/Dropdown/Dropdown.vue
+++ packages/vue3/src/components/Dropdown/Dropdown.vue
@@ -1,49 +1,13 @@
 <script setup lang="ts">
-import { computed, ref, watchEffect } from 'vue';
-import SelectButton from '~/components/_Select/SelectButton.vue';
-import SelectMenu from '~/components/_Select/SelectMenu.vue';
-import type { SelectItem } from '~/components/_Select/types';
 
-const { modelValue, items, placeholder } = defineProps<{
-  items: SelectItem[]
-  modelValue: string | undefined
-  placeholder?: string
-}>();
-
-const emit = defineEmits(['update:modelValue']);
-
-const selected = computed(() => modelValue);
-const selectedItem = ref<SelectItem>();
-const shown = ref(false);
-
-const itemSelected = (item: SelectItem | undefined): void => {
-  if (!item) {
-    return;
-  }
-
-  selectedItem.value = item;
-  emit('update:modelValue', selectedItem.value.value);
-
-  shown.value = false;
-};
-
-watchEffect(() => selectedItem.value = items.find(item => item.value === selected.value));
 </script>
 
 <template>
-  <div class="dropdown">
-    <SelectButton
-      :item="selectedItem" :placeholder="placeholder" @click="shown = !shown"
-      @clickOutside="shown = false"
-    />
-    <SelectMenu v-show="shown" :model-value="selectedItem" :items="items" @update:modelValue="itemSelected" />
+  <div>
+    a
   </div>
 </template>
 
-<style lang="scss" scoped>
-.dropdown {
-  display: flex;
-  flex-direction: column;
-  gap: 7px;
-}
+<style scoped>
+
 </style>
