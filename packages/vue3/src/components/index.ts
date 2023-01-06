d40f50cdba4c509c0a8fc4b30e8718ee6959cb9e
Ali HMIMS <85164582+AliHMIMS@users.noreply.github.com>
2023-01-06 09:13:55 +0100

    init component

--- packages/vue3/src/components/index.ts
+++ packages/vue3/src/components/index.ts
@@ -38,3 +38,4 @@ export { default as Topbar } from './Topbar/Topbar.vue';
 export { default as ActionBar } from './ActionBar/ActionBar.vue';
 export { default as SettingsNav } from './SettingsNav/SettingsNav.vue';
 export { default as SettingsNavItem } from './SettingsNav/SettingsNavItem.vue';
+export { default as Table } from './Table/Table.vue';
