3eb81169b02ab6862cd2ad47adb68a2c2432cc1d
Ali HMIMS <85164582+AliHMIMS@users.noreply.github.com>
2023-01-09 16:38:08 +0100

    Vmodeled table

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -2,24 +2,59 @@
 import { ref } from 'vue';
 import '~/assets/main.css';
 import { EditableStatus, StaticStatus } from '~/components';
+import Table from '~/components/Table/Table.vue';
+import type { TableColumn, TableData } from '~/components/Table/types';
 
 const status = ref({ label: 'Success', value: 1, color: '#25B86A' });
 
-const statuses = [
-  { label: 'What the dog doing', value: 3, color: '#127EE3' },
-  { label: 'Error', value: 2, color: '#CC2929' },
-  { label: 'Warning', value: 4, color: '#F2990E' },
-  { label: 'Success', value: 1, color: '#25B86A' },
+const columns: TableColumn[] = [
+  { label: '', accessor: 'image' },
+  { label: 'Name', accessor: 'name', sortable: 'desc' },
+  { label: 'Age', accessor: 'age' },
+  { label: 'Email', accessor: 'email' },
+  { label: 'Profile', accessor: 'profile' },
 ];
+
+const data = ref<TableData[]>(
+  [
+    {
+      name: 'John Doe',
+      age: {
+        variant: 'status',
+        data: {
+          statuses: [{ label: 'Old', color: 'red', value: 42 }, { label: 'Young', color: 'green', value: 22 }],
+          modelValue: { label: 'Young', color: 'green', value: 22 },
+        },
+      },
+      email: 'Doe@gmail.com',
+    },
+    {
+      name: 'Ali baba',
+      image: {
+        variant: 'thumbnail',
+        data: {
+          size: 'small',
+        },
+      },
+      profile: {
+        variant: 'link',
+        data: {
+          href: 'https://www.google.com',
+          label: 'Facebook',
+        },
+      },
+      age: 42,
+      email: 'Doe@gmail.com',
+    },
+    { name: 'John Smith', age: 22, email: 'Smith@gmail.com' },
+  ],
+);
 </script>
 
 <template>
   <div>
-    <StaticStatus :status="status" />
-
-    <br>
-
-    <EditableStatus v-model="status" :statuses="statuses" />
+    <Table v-model:data="data" :columns="columns" />
+    <textarea id="" name="" cols="30" rows="10" :value="JSON.stringify(data)" />
   </div>
 </template>
 
--- packages/vue3/src/components/Table/Table.vue
+++ packages/vue3/src/components/Table/Table.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, shallowRef } from 'vue';
 import type { TableColumn, TableColumnValue, TableData, TableDataComposable } from './types';
 import ColumnRegistrar from './Internal/cr';
 import { launder } from '~/utils/type.util';
@@ -11,6 +11,7 @@ const props = defineProps<{
 
 const emit = defineEmits<{
   (event: 'sort', column: TableColumn, index: number): void
+  (event: 'update:data', data: TableData[]): void
 }>();
 
 const rows = computed(
@@ -38,7 +39,49 @@ const rows = computed(
 
 const emitSort = (column: TableColumn, index: number) => emit('sort', column, index);
 
-const loggg = (x: any, y: any) => console.log(x, y);
+function ougaBouga(ddd: Record<keyof TableData, TableColumnValue>[]): TableData[] {
+  const yy: TableData[] = props.data.map((row: TableData, index: number) => {
+    const rowObject: TableData = {};
+
+    Object.keys(row).forEach((key: keyof TableData) => {
+      const composedRow = rows.value[index][key];
+      const propRow = row[key];
+
+      if (typeof composedRow === 'undefined' || typeof propRow !== 'object') {
+        rowObject[key] = propRow;
+
+        return;
+      }
+
+      rowObject[key] = ddd[index][key].value;
+    });
+
+    return rowObject;
+  });
+
+  return yy;
+}
+
+function handleSubCompModel(row: number, accessor: string, data: unknown) {
+  console.log(row, accessor, data);
+
+  const ffff = shallowRef(rows.value);
+  const propRow = props.data[row][accessor] as TableDataComposable;
+
+  ffff.value[row][accessor].value = {
+    // @ts-expect-error - TS is crying about variant type here, but it's not a problem since it's valid
+    variant: propRow.variant,
+    data: {
+      ...propRow.data,
+      // @ts-expect-error - Expected from TS to not know the type def here since the value is dynamically set
+      modelValue: data,
+    },
+  };
+
+  console.log(ougaBouga(ffff.value), props.data);
+
+  emit('update:data', ougaBouga(ffff.value));
+}
 </script>
 
 <template>
@@ -58,7 +101,8 @@ const loggg = (x: any, y: any) => console.log(x, y);
             <template v-if="row[column.accessor]">
               <span v-if="row[column.accessor].isString" class="text-column">{{ row[column.accessor].value }}</span>
               <component :is="row[column.accessor].component" v-else-if="!row[column.accessor].isString"
-                v-bind="launder<TableDataComposable>(row[column.accessor].value).data" v-on="loggg" />
+                v-bind="launder<TableDataComposable>(row[column.accessor].value).data"
+                @update:model-value="(data: unknown) => handleSubCompModel(index, column.accessor, data)" />
             </template>
           </td>
         </tr>
