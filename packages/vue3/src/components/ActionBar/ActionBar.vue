1dcee0535535d23ccedefc9e872ca6cadcd4d8aa
Aymane D. HLAMNACH <aymane.hlamnach@youcan.shop>
2023-01-09 14:50:36 +0100

    7113

---
+++ packages/vue3/src/components/ActionBar/ActionBar.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="action-bar">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.action-bar {
+    padding: 12px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+}
+</style>
