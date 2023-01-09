a9f5665eda24ef406a90a50515f6c6591cfc05aa
Aymane D. Hlamnach <95413644+azuradarius@users.noreply.github.com>
2023-01-09 10:28:06 +0100

    DOT-7105: Input dropdown prefix (#70)

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -1,28 +1,77 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import '~/assets/main.css';
-import { EditableStatus, StaticStatus } from '~/components';
+import { DropdownPrefix, Input, InputGroup } from '~/components';
+import type { DropdownItemGroups } from '~/components/Dropdown/types';
 
-const status = ref({ label: 'Success', value: 1, color: '#25B86A' });
+const items = {
+  One: [
+    {
+      value: 1,
+      image:
+        'https://wp.clutchpoints.com/wp-content/uploads/2021/07/There_s-an-entire-JRPG-on-Google_s-banner-to-celebrate-Tokyo-2020.jpg',
+      label: 'Monkey island',
+    },
+    {
+      value: 2,
+      image:
+        'https://wp.clutchpoints.com/wp-content/uploads/2021/07/There_s-an-entire-JRPG-on-Google_s-banner-to-celebrate-Tokyo-2020.jpg',
+      label: 'Monkey island 2',
+    },
+    {
+      value: 3,
+      image:
+        'https://wp.clutchpoints.com/wp-content/uploads/2021/07/There_s-an-entire-JRPG-on-Google_s-banner-to-celebrate-Tokyo-2020.jpg',
+      label: 'Monkey island 3',
+    },
+  ],
+  Two: [
+    {
+      value: 4,
+      image:
+        'https://wp.clutchpoints.com/wp-content/uploads/2021/07/There_s-an-entire-JRPG-on-Google_s-banner-to-celebrate-Tokyo-2020.jpg',
+      label: 'Monkey island 4',
+    },
+    {
+      value: 5,
+      image:
+        'https://wp.clutchpoints.com/wp-content/uploads/2021/07/There_s-an-entire-JRPG-on-Google_s-banner-to-celebrate-Tokyo-2020.jpg',
+      label: 'Monkey island 5',
+    },
+  ],
+} as DropdownItemGroups;
 
-const statuses = [
-  { label: 'What the dog doing', value: 3, color: '#127EE3' },
-  { label: 'Error', value: 2, color: '#CC2929' },
-  { label: 'Warning', value: 4, color: '#F2990E' },
-  { label: 'Success', value: 1, color: '#25B86A' },
-];
+const model = ref({ main: '', prefix: null });
 </script>
 
 <template>
   <div>
-    <StaticStatus :status="status" />
+    <InputGroup style="max-width: 500px">
+      <template #input>
+        <Input v-model="model.main" type="text" placeholder="Translate text..">
+          <template #icon>
+            <i i-youcan-language />
+          </template>
 
-    <br>
+          <template #suffix>
+            km/h
+          </template>
 
-    <EditableStatus v-model="status" :statuses="statuses" />
+          <template #prefix>
+            <DropdownPrefix v-model="model.prefix" icon="i-youcan-language" placeholder="Language" :items="items" />
+          </template>
+        </Input>
+      </template>
+
+      <template #label>
+        Language picker
+      </template>
+
+      <template #info>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio, qui?
+      </template>
+    </InputGroup>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
