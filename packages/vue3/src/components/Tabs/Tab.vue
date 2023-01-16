1050a7183ccf24cb51f8b0ac735b60967ed3820e
Ali HMIMS <85164582+AliHMIMS@users.noreply.github.com>
2023-01-16 15:52:34 +0100

    Update Tab.vue

--- packages/vue3/src/components/Tabs/Tab.vue
+++ packages/vue3/src/components/Tabs/Tab.vue
@@ -22,66 +22,66 @@ defineProps<{
 
 <style scoped>
 button {
-    border: none;
-    cursor: pointer;
-    background-color: transparent;
+  border: none;
+  cursor: pointer;
+  background-color: transparent;
 }
 
 button:is(:focus, :active) {
-    outline: none;
+  outline: none;
 }
 
 .tab {
-    gap: 4px;
-    display: flex;
-    padding: 16px 0;
-    align-items: center;
-    margin-bottom: -1px;
-    justify-content: center;
-    border-bottom: 1px solid transparent;
+  gap: 4px;
+  display: flex;
+  padding: 16px 0;
+  align-items: center;
+  margin-bottom: -1px;
+  justify-content: center;
+  border-bottom: 1px solid transparent;
 }
 
 .tab .label {
-    border-radius: 4px;
-    color: var(--gray-700);
-    font: var(--text-md-medium);
+  border-radius: 4px;
+  color: var(--gray-700);
+  font: var(--text-md-regular);
 }
 
 .tab:hover .label {
-    color: var(--gray-900);
+  color: var(--gray-900);
 }
 
 .tab:is(:focus, :active):not(.active, .disabled) .label {
-    box-shadow: var(--focus-xs-brand);
-    outline: 1px solid var(--brand-500);
+  box-shadow: var(--focus-xs-brand);
+  outline: 1px solid var(--brand-500);
 }
 
 .tab.active {
-    cursor: default;
-    border-bottom: 2px solid var(--brand-500);
+  cursor: default;
+  border-bottom: 2px solid var(--brand-500);
 }
 
 .tab.active .label {
-    color: var(--brand-500);
+  color: var(--brand-500);
 }
 
 .tab.error .label {
-    color: var(--red-500);
+  color: var(--red-500);
 }
 
 .tab.error:hover .label {
-    color: var(--red-600);
+  color: var(--red-600);
 }
 
 .tab.error.active {
-    border-bottom: 1px solid var(--red-500);
+  border-bottom: 1px solid var(--red-500);
 }
 
 .tab.disabled {
-    pointer-events: none;
+  pointer-events: none;
 }
 
 .tab.disabled .label {
-    color: var(--gray-300);
+  color: var(--gray-300);
 }
 </style>
