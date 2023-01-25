7f019532e6e6852155acdd49e1fddaa5755abe94
Aymane D. Hlamnach <95413644+azuradarius@users.noreply.github.com>
2023-01-25 10:38:57 +0100

    dark: dynamic imports fix (#88)

--- packages/vue3/src/components/Table/Internal/cells-registrar.ts
+++ packages/vue3/src/components/Table/Internal/cells-registrar.ts
@@ -1,19 +1,28 @@
 import type { Component } from 'vue';
-import { defineAsyncComponent } from 'vue';
 import type { TableComposableVariant } from '../types';
+import Link from './Link.vue';
+import Button from './Button.vue';
+import Thumbnail from '~/components/Thumbnail/Thumbnail.vue';
+import EditableStatus from '~/components/Status/EditableStatus.vue';
+import Percentage from '~/components/Percentage/Percentage.vue';
+import Rating from '~/components/Rating/Rating.vue';
+import Toggle from '~/components/Toggle/Toggle.vue';
+import Increment from '~/components/Increment/Increment.vue';
+import Icon from '~/components/Table/Internal/Icon.vue';
+import StaticStatus from '~/components/Status/StaticStatus.vue';
 
 export default function (name: TableComposableVariant): Component | undefined {
   switch (name) {
-    case 'thumbnail': return defineAsyncComponent(() => import('~/components/Thumbnail/Thumbnail.vue'));
-    case 'link': return defineAsyncComponent(() => import('~/components/Table/Internal/Link.vue'));
-    case 'status': return defineAsyncComponent(() => import('~/components/Status/EditableStatus.vue'));
-    case 'static-status': return defineAsyncComponent(() => import('~/components/Status/StaticStatus.vue'));
-    case 'percentage': return defineAsyncComponent(() => import('~/components/Percentage/Percentage.vue'));
-    case 'button': return defineAsyncComponent(() => import('~/components/Table/Internal/Button.vue'));
-    case 'rating': return defineAsyncComponent(() => import('~/components/Rating/RatingInput.vue'));
-    case 'toggle': return defineAsyncComponent(() => import('~/components/Toggle/Toggle.vue'));
-    case 'counter': return defineAsyncComponent(() => import('~/components/Increment/Increment.vue'));
-    case 'icon': return defineAsyncComponent(() => import('~/components/Table/Internal/Icon.vue'));
+    case 'thumbnail': return Thumbnail;
+    case 'link': return Link;
+    case 'status': return EditableStatus;
+    case 'static-status': return StaticStatus;
+    case 'percentage': return Percentage;
+    case 'button': return Button;
+    case 'rating': return Rating;
+    case 'toggle': return Toggle;
+    case 'counter': return Increment;
+    case 'icon': return Icon;
     default: return undefined;
   }
 }
