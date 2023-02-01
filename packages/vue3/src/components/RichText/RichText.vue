49a53eda3b2bfbdb4a32a8d0e071faf85fa717ea
ybenadda <117163420+ybenadda@users.noreply.github.com>
2023-02-01 17:37:18 +0100

    feat(DOT-7263): add necessary actions

--- packages/vue3/src/components/RichText/RichText.vue
+++ packages/vue3/src/components/RichText/RichText.vue
@@ -4,15 +4,14 @@ import StarterKit from '@tiptap/starter-kit';
 import { onBeforeUnmount } from 'vue';
 import Underline from '@tiptap/extension-underline';
 import HorizontalRule from '@tiptap/extension-horizontal-rule';
-import Iframe from './extensions/iframe';
 
 const editor = useEditor({
   content: '<p>I’m running Tiptap with Vue.js. 🎉</p>',
   extensions: [
-    Iframe,
     StarterKit,
     Underline,
     HorizontalRule,
+
   ],
 });
 
@@ -20,25 +19,32 @@ onBeforeUnmount(() => {
   editor.value?.destroy();
 });
 
-const toolbar = {
-  bold: editor.value?.chain().focus().toggleBold().run(),
-  italic: editor.value?.chain().focus().toggleItalic().run(),
-  strike: editor.value?.chain().focus().toggleStrike().run(),
-  code: editor.value?.chain().focus().toggleCodeBlock().run(),
-  underline: editor.value?.chain().focus().setUnderline().run(),
-  hr: editor.value?.chain().focus().setHorizontalRule().run(),
-  clearMarks: editor.value?.chain().focus().unsetAllMarks().run(),
-  clearNodes: editor.value?.chain().focus().clearNodes().run(),
-  undo: editor.value?.chain().focus().undo().run(),
-  redo: editor.value?.chain().focus().redo().run(),
+const toolbar: Record<string, (_?: any) => void> = {
+  bold: () => editor.value?.chain().focus().toggleBold().run(),
+  italic: () => editor.value?.chain().focus().toggleItalic().run(),
+  strike: () => editor.value?.chain().focus().toggleStrike().run(),
+  code: () => editor.value?.chain().focus().toggleCodeBlock().run(),
+  underline: () => editor.value?.chain().focus().setUnderline().run(),
+  hr: () => editor.value?.chain().focus().setHorizontalRule().run(),
+  clearMarks: () => editor.value?.chain().focus().unsetAllMarks().run(),
+  clearNodes: () => editor.value?.chain().focus().clearNodes().run(),
+  undo: () => editor.value?.chain().focus().undo().run(),
+  redo: () => editor.value?.chain().focus().redo().run(),
   heading: (level: Level) => editor.value?.chain().focus().toggleHeading({ level }).run(),
-  ul: editor.value?.chain().focus().toggleBulletList().run(),
-  ol: editor.value?.chain().focus().toggleOrderedList().run(),
-  iframe: (url: string) => editor.value?.chain().focus().setIframe({ src: url }).run(),
+  ul: () => editor.value?.chain().focus().toggleBulletList().run(),
+  ol: () => editor.value?.chain().focus().toggleOrderedList().run(),
+//   iframe: (url: string) => editor.value?.chain().focus().setIframe({ src: url }).run(),
 };
+
+const run = (action: string) => toolbar[action]();
 </script>
 
 <template>
   <h1>Hello</h1>
+  <div>
+    <div v-for="el in Object.keys(toolbar)" :key="el" @click="run(el)">
+      {{ el }}
+    </div>
+  </div>
   <EditorContent :editor="editor" />
 </template>
