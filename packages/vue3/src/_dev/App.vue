d02d1d525cccc05ccda9b34cad1a479bd9771042
jamyoucn00 <114928221+jamyoucn00@users.noreply.github.com>
2023-02-02 12:26:16 +0100

    update actions

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -121,77 +121,7 @@ const actions: TableActions[] = [
       click: () => {
         console.log('delete');
       },
-      {
-        label: 'sidebar.orders.new',
-        href: endpointSA('orders.create'),
-      },
-      {
-        label: 'sidebar.orders.abandoned-carts',
-        href: endpointSA('abandoned-cart.dashboard'),
-      },
-    ],
-  },
-  {
-    label: 'sidebar.products.title',
-    icon: 'i-youcan-tag',
-    href: '',
-    children: [
-      {
-        label: 'sidebar.products.all',
-        href: endpointSA('products.index'),
-      },
-      {
-        label: 'sidebar.products.new',
-        href: endpointSA('products.create'),
-      },
-      {
-        label: 'sidebar.products.categories',
-        href: endpointSA('categories.index'),
-      },
-      {
-        label: 'sidebar.products.reviews',
-        href: endpointSA('products.reviews.index'),
-      },
-      {
-        label: 'sidebar.products.inventories',
-        href: endpointSA('products.inventory.index'),
-      },
-    ],
-  },
-  {
-    label: 'sidebar.upsells',
-    icon: 'i-youcan-caret-double-up',
-    href: '/upsells',
-  },
-  {
-    label: 'sidebar.coupons',
-    icon: 'i-youcan-ticket',
-    href: '/coupons',
-  },
-  {
-    label: 'sidebar.customers',
-    icon: 'i-youcan-users',
-    href: '/customers',
-  },
-  {
-    label: 'sidebar.store',
-    icon: 'i-youcan-storefront',
-    href: '/store',
-  },
-  {
-    label: 'sidebar.insights',
-    icon: 'i-youcan-chart-line',
-    href: '/insights',
-  },
-  {
-    label: 'sidebar.invoices',
-    icon: 'i-youcan-receipt',
-    href: '/invoices',
-  },
-  {
-    label: 'sidebar.apps',
-    icon: 'i-youcan-squares-four',
-    href: '/apps',
+    },
   },
 ];
 
