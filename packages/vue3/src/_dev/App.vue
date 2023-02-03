baef89f1aa7034cd6decfcbb03202b6e50b09fef
Ali HMIMS <85164582+AliHMIMS@users.noreply.github.com>
2023-02-03 09:06:59 +0100

    [table]: Introduce child rows support (#90)
    Co-authored-by: jamyoucn00 <114928221+jamyoucn00@users.noreply.github.com>

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -1,99 +1,127 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import '~/assets/main.css';
-import { EditableStatus, Info, PrimaryButton, Sidebar, SidebarItem, SidebarSubitem, StaticStatus, TertiaryButton } from '~/components';
+import { EditableStatus, StaticStatus } from '~/components';
 import Table from '~/components/Table/Table.vue';
 import type { TableActions, TableColumn, TableData } from '~/components/Table/types';
-import Input from '~/components/Input/Input.vue';
 
 const endpointSA = (text: string) => text;
 
-const items = [
-  {
-    label: 'sidebar.dashboard',
-    icon: 'i-youcan-house-simple',
-    href: endpointSA('dashboard'),
-  },
-  {
-    label: 'sidebar.orders.title',
-    icon: 'i-youcan-cube',
-    href: '',
-    children: [
-      {
-        label: 'sidebar.orders.all',
-        href: endpointSA('orders.index'),
+const columns: TableColumn[] = [
+  { label: '', accessor: 'image' },
+  { label: 'Name', accessor: 'name', sortable: 'desc' },
+  { label: 'Age', accessor: 'age' },
+  { label: 'Email', accessor: 'email' },
+  { label: 'Profile', accessor: 'profile' },
+];
+
+const data = ref<TableData[]>(
+  [
+    {
+      row: {
+        image: {
+          variant: 'button',
+          data: {
+            label: 'Call to action',
+            // iconName: 'i-youcan-x',
+          },
+          events: {
+            click: () => {
+              console.log('click');
+            },
+          },
+        },
+        profile: {
+          variant: 'rating',
+          data: {
+            modelValue: 3,
+          },
+        },
+        name: 'John Doe',
+        age: {
+          variant: 'status',
+          data: {
+            statuses: [{ label: 'Old', color: 'red', value: 42 }, { label: 'Young', color: 'green', value: 22 }],
+            modelValue: { label: 'Young', color: 'green', value: 22 },
+          },
+        },
+        email: 'Doe@gmail.com',
       },
-      {
-        label: 'sidebar.orders.new',
-        href: endpointSA('orders.create'),
+      children: [
+        {
+          image: {
+            variant: 'button',
+            data: {
+              label: 'Call to action',
+              // iconName: 'i-youcan-x',
+            },
+            events: {
+              click: () => {
+                console.log('click');
+              },
+            },
+          },
+          profile: {
+            variant: 'rating',
+            data: {
+              modelValue: 3,
+            },
+          },
+          name: 'John Doe',
+          age: {
+            variant: 'status',
+            data: {
+              statuses: [{ label: 'Old', color: 'red', value: 42 }, { label: 'Young', color: 'green', value: 22 }],
+              modelValue: { label: 'Young', color: 'green', value: 22 },
+            },
+          },
+          email: 'Doe@gmail.com',
+        },
+      ],
+    },
+    {
+      row: {
+        image: {
+          variant: 'button',
+          data: {
+            label: 'Call to action',
+            // iconName: 'i-youcan-x',
+          },
+          events: {
+            click: () => {
+              console.log('click');
+            },
+          },
+        },
+        profile: {
+          variant: 'rating',
+          data: {
+            modelValue: 3,
+          },
+        },
+        name: 'John Doe',
+        age: {
+          variant: 'status',
+          data: {
+            statuses: [{ label: 'Old', color: 'red', value: 42 }, { label: 'Young', color: 'green', value: 22 }],
+            modelValue: { label: 'Young', color: 'green', value: 22 },
+          },
+        },
+        email: 'Doe@gmail.com',
       },
-      {
-        label: 'sidebar.orders.abandoned-carts',
-        href: endpointSA('abandoned-cart.dashboard'),
+    },
+  ],
+);
+
+const actions: TableActions[] = [
+  {
+    label: 'Delete',
+    iconName: 'i-youcan-trash',
+    events: {
+      click: () => {
+        console.log('delete');
       },
-    ],
-  },
-  {
-    label: 'sidebar.products.title',
-    icon: 'i-youcan-tag',
-    href: '',
-    children: [
-      {
-        label: 'sidebar.products.all',
-        href: endpointSA('products.index'),
-      },
-      {
-        label: 'sidebar.products.new',
-        href: endpointSA('products.create'),
-      },
-      {
-        label: 'sidebar.products.categories',
-        href: endpointSA('categories.index'),
-      },
-      {
-        label: 'sidebar.products.reviews',
-        href: endpointSA('products.reviews.index'),
-      },
-      {
-        label: 'sidebar.products.inventories',
-        href: endpointSA('products.inventory.index'),
-      },
-    ],
-  },
-  {
-    label: 'sidebar.upsells',
-    icon: 'i-youcan-caret-double-up',
-    href: '/upsells',
-  },
-  {
-    label: 'sidebar.coupons',
-    icon: 'i-youcan-ticket',
-    href: '/coupons',
-  },
-  {
-    label: 'sidebar.customers',
-    icon: 'i-youcan-users',
-    href: '/customers',
-  },
-  {
-    label: 'sidebar.store',
-    icon: 'i-youcan-storefront',
-    href: '/store',
-  },
-  {
-    label: 'sidebar.insights',
-    icon: 'i-youcan-chart-line',
-    href: '/insights',
-  },
-  {
-    label: 'sidebar.invoices',
-    icon: 'i-youcan-receipt',
-    href: '/invoices',
-  },
-  {
-    label: 'sidebar.apps',
-    icon: 'i-youcan-squares-four',
-    href: '/apps',
+    },
   },
 ];
 
@@ -118,22 +146,10 @@ const subItems = [
 
 <template>
   <div>
-    <TertiaryButton size="sm">
-      Primary
-    </TertiaryButton>
-    <Sidebar>
-      <template #header>
-        Hello
-      </template>
-      <template #items>
-        <SidebarItem v-for="item in items" :key="item.href" :icon="item.icon" :label="item.label">
-          <template v-if="item.children">
-            <SidebarSubitem v-for="subItem in item.children" :key="subItem.href" :label="subItem.label" />
-          </template>
-        </SidebarItem>
-      </template>
-    </Sidebar>
-    <Info />
+    <Table v-model:data="data" v-model:selected-rows="selectedRows" :columns="columns" :actions="actions"
+      :selectable="true" />
+    <textarea id="" name="" cols="100" rows="10" :value="JSON.stringify(data)" />
+    <textarea id="" name="" cols="100" rows="10" :value="JSON.stringify(selectedRows)" />
   </div>
 </template>
 
---
+++ packages/vue3/src/components/Table/Internal/TableRow.vue
@@ -0,0 +1,191 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import type { TableActions, TableColumn, TableColumnValue, TableData, TableDataComposable, TableDataRow, TableInternalData } from '../types';
+import TableButton from './Button.vue';
+import TertiaryButton from '~/components/Button/TertiaryButton.vue';
+import Checkbox from '~/components/Checkbox/Checkbox.vue';
+import { launder } from '~/utils/type.util';
+
+const props = defineProps<{
+  row: TableInternalData
+  columns: TableColumn[]
+  actions?: TableActions[]
+  index: number
+  selected?: boolean
+  expended?: boolean
+  isChild?: boolean
+  data: TableData[]
+}>();
+
+const emit = defineEmits<{
+  (event: 'update:selected-rows', data: boolean): void
+  (event: 'update:expend', data: boolean): void
+  (event: 'update:sub-comp-model', data: { index: number; accessor: string; data: unknown; child?: boolean }): void
+}>();
+
+const isSelected = computed({
+  get: () => props.selected,
+  set: (value) => {
+    emit('update:selected-rows', value!);
+  },
+});
+
+const isExpended = computed({
+  get: () => props.expended,
+  set: (value) => {
+    emit('update:expend', value!);
+  },
+});
+
+const handleSubCompModel = (index: number, accessor: string, data: unknown, child: boolean) => {
+  emit('update:sub-comp-model', {
+    index,
+    accessor,
+    data,
+    child,
+  });
+};
+
+const checkActionCriteria = (action: TableActions, data: TableData) => {
+  return !action.criteria || action.criteria(data);
+};
+</script>
+
+<template>
+  <tr class="table-row" :class="{ 'selected': isSelected, 'expended-child': isChild, 'expended-wrapper': expended }">
+    <td v-for="column in columns" :key="column.accessor" class="table-cell">
+      <div v-if="column.accessor === 'check'" class="row-checker">
+        <Checkbox v-model="isSelected" />
+      </div>
+      <template v-else-if="column.accessor === '_expand' && row.children && row.children.length > 0">
+        <TertiaryButton
+          size="xs" icon-position="only" rounded-full
+          @click="isExpended = !isExpended"
+        >
+          <template #icon>
+            <i class="" :class="[isExpended ? 'i-youcan-carret-down' : 'i-youcan-caret-right']" />
+          </template>
+        </TertiaryButton>
+      </template>
+      <template v-else-if="row.row[column.accessor]">
+        <div
+          v-if="row.row[column.accessor].isString" class="text-column"
+          :class="{ na: row.row[column.accessor].value.toString().toLocaleLowerCase() === 'n/a' }"
+        >
+          {{ row.row[column.accessor].value }}
+        </div>
+        <component
+          :is="row.row[column.accessor].component" v-else-if="!row.row[column.accessor].isString"
+          v-bind="launder<TableDataComposable>(row.row[column.accessor].value).data"
+          @update:model-value="(data: unknown) => handleSubCompModel(index, column.accessor, data, data.child)"
+          v-on="launder<TableDataComposable>(row.row[column.accessor].value).events || {}"
+        />
+      </template>
+      <div v-if="column.accessor === 'actions' && actions?.length" class="cell-actions">
+        <template v-for="action in actions" :key="action.label">
+          <!-- v-if="!action.criteria || action.criteria(data[index])" -->
+          <TableButton
+            v-if="checkActionCriteria(action, data[index])"
+            size="xs"
+            :data="data"
+            :icon-name="action.iconName"
+            :label="action.label"
+            :rounded-full="true"
+            icon-position="only"
+            v-on="action.events || {}"
+          />
+        </template>
+      </div>
+    </td>
+  </tr>
+  <template v-if="isExpended && row">
+    <TableRow
+      v-for="(child, i) in row.children"
+      :key="i"
+      :index="i"
+      :row="child"
+      :data="data"
+      is-child
+      :columns="columns"
+      :actions="actions"
+      @update:sub-comp-model="(data) => handleSubCompModel(i, data.accessor, data.data, true)"
+    />
+  </template>
+</template>
+
+<style scoped>
+.table-row {
+  background-color: var(--base-white);
+  height: 68px;
+  border-bottom: 1px solid var(--gray-100);
+}
+
+.table-row.selected {
+  background-color: var(--brand-50);
+}
+
+.table-row.selected .table-cell:first-of-type {
+  position: relative;
+}
+
+.table-row.selected .table-cell:first-of-type::before {
+  content: '';
+  display: block;
+  position: absolute;
+  left: 0;
+  top: 0;
+  background-color: var(--brand-500);
+  width: 3px;
+  height: calc(100% + 1px);
+}
+
+.table-row .table-cell {
+  padding: 0 12px;
+}
+
+.table-row .table-cell .cell-actions {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+}
+
+.table-row .table-cell .cell-actions .secondary {
+  --icon-color: var(--gray-500);
+}
+
+.table-row .table-cell .row-checker {
+  padding-left: 4px;
+}
+
+.table-row .table-cell .percentage {
+  width: fit-content;
+}
+
+.table-row .table-cell .increment {
+  --width: max-content;
+}
+
+.table-row .table-cell .increment:deep(input) {
+  width: 30px;
+}
+
+.table-row.expended-child {
+  background-color: var(--gray-50);
+}
+
+.table-row.expended-wrapper {
+  box-shadow: var(--shadow-sm-gray);
+  position: relative;
+}
+
+.text-column {
+  font: var(--text-sm-regular);
+  color: var(--gray-900);
+}
+
+.text-column.na {
+  font: var(--text-sm-medium);
+  color: var(--gray-300);
+  text-transform: uppercase;
+}
+</style>
