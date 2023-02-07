e6370a36d3f61b44444350af48b10d7e4cfb2753
ybenadda <117163420+ybenadda@users.noreply.github.com>
2023-02-07 15:34:00 +0100

    wip: upload image

--- packages/vue3/src/components/RichText/handleDrop.ts
+++ packages/vue3/src/components/RichText/handleDrop.ts
@@ -1,9 +1,29 @@
 import type { Slice } from 'prosemirror-model';
 import type { EditorView } from 'prosemirror-view';
 
+const UPLOAD_IMAGE_BASE_URL = 'https://seller-area.youcan.shop/admin/upload/file/others';
+
 const MAX_IMAGE_SIZE_MB = 10;
 const MAX_IMAGE_WIDTH = 2000;
 const MAX_IMAGE_HEIGHT = 2000;
+
+async function uploadImage(file: File): Promise<string> {
+  const data = new FormData();
+  data.append('image', file);
+  const response = await fetch(UPLOAD_IMAGE_BASE_URL, {
+    method: 'POST',
+    body: data,
+  });
+
+  const responseData = await response.json();
+
+  if (response.ok) {
+    const { link } = responseData;
+    return link;
+  }
+
+  throw new Error('failed to upload image');
+}
 
 export default function (view: EditorView, event: DragEvent, slice: Slice, moved: boolean) {
   if (!moved && event.dataTransfer && event.dataTransfer.files && event.dataTransfer.files[0]) {
@@ -13,14 +33,15 @@ export default function (view: EditorView, event: DragEvent, slice: Slice, moved
       const _URL = window.URL || window.webkitURL;
       const img = new Image();
       img.src = _URL.createObjectURL(file);
-      img.onload = function () {
+      img.onload = async function () {
         if (this.width > MAX_IMAGE_WIDTH || this.height > MAX_IMAGE_HEIGHT) {
           alert(`image not valid max image size is ${MAX_IMAGE_WIDTH} x ${MAX_IMAGE_HEIGHT}`);
         }
         else {
+          const src = await uploadImage(file);
           const { schema } = view.state;
           const coordinates = view.posAtCoords({ left: event.clientX, top: event.clientY })!;
-          const node = schema.nodes.image.create({ src: img.src });
+          const node = schema.nodes.image.create({ src });
           const transaction = view.state.tr.insert(coordinates.pos, node);
           return view.dispatch(transaction);
         }
