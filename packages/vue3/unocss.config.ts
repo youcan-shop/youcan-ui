4c8399c5478397ee66f5711617886ce0d93044bf
ybenadda <117163420+ybenadda@users.noreply.github.com>
2023-02-09 17:47:02 +0100

    feat: add visuals

--- packages/vue3/unocss.config.ts
+++ packages/vue3/unocss.config.ts
@@ -5,8 +5,8 @@ import {
 } from 'unocss';
 import YouCanIconsInfo from '@youcan/ui-icons/info.json';
 import YouCanIcons from '@youcan/ui-icons/icons.json';
-import YouCanVisualsInfo from '@/youcan/ui-visuals/info.json';
-import YouCanVisuals from '@/youcan/ui-visuals/info.json';
+import YouCanVisualsInfo from '@youcan/ui-visuals/info.json';
+import YouCanVisuals from '@youcan/ui-visuals/icons.json';
 
 export default defineConfig({
   presets: [
