432a9d6468515729d7b25513c81df0da77292cb4
ybenadda <117163420+ybenadda@users.noreply.github.com>
2023-02-09 16:09:19 +0100

    init

--- packages/vue3/unocss.config.ts
+++ packages/vue3/unocss.config.ts
@@ -5,6 +5,8 @@ import {
 } from 'unocss';
 import YouCanIconsInfo from '@youcan/ui-icons/info.json';
 import YouCanIcons from '@youcan/ui-icons/icons.json';
+import YouCanVisualsInfo from '@/youcan/ui-visuals/info.json';
+import YouCanVisuals from '@/youcan/ui-visuals/info.json';
 
 export default defineConfig({
   presets: [
@@ -18,6 +20,7 @@ export default defineConfig({
       },
       collections: {
         [YouCanIconsInfo.prefix || 'youcan']: () => YouCanIcons,
+        [YouCanVisualsInfo.prefix || 'visuals']: () => YouCanVisuals,
       },
     }),
   ],
