08421efd9d04551cee4913d2f34b9cc61f3ed2dd
Ali HMIMS <85164582+AliHMIMS@users.noreply.github.com>
2023-02-13 11:41:22 +0100

    Draggable container

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -1,17 +1,29 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import '~/assets/main.css';
-import { DraggableItem } from '~/components';
+import { Draggable, DraggableItem } from '~/components';
 
 const helloKitty = ref({
   label: 'Order ID',
   value: 'order_id',
   selected: false,
 });
+
+const helloKitties = ref([{
+  label: 'Order ID',
+  value: 'order_id',
+  selected: false,
+}, {
+  label: 'Order ID 2',
+  value: 'order_id',
+  selected: false,
+}]);
 </script>
 
 <template>
   <div>
     <DraggableItem v-model="helloKitty" />
+    <Draggable v-model="helloKitties" />
+    <textarea cols="30" v-text="JSON.stringify(helloKitties)" />
   </div>
 </template>
--- packages/vue3/src/components/Draggable/Draggable.vue
+++ packages/vue3/src/components/Draggable/Draggable.vue
@@ -1,11 +1,49 @@
 <script setup lang="ts">
+import { computed } from 'vue';
+import draggable from 'vuedraggable';
+import DraggableItem from './DraggableItem.vue';
 import type { DraggableItemType } from './types';
 
-defineProps<{
+const props = defineProps<{
   modelValue: DraggableItemType[]
 }>();
+
+const emit = defineEmits<{
+  (event: 'update:model-value', modelValue: DraggableItemType[]): void
+}>();
+
+const model = computed({
+  get: () => props.modelValue,
+  set: (value: DraggableItemType[]) => emit('update:model-value', value),
+});
 </script>
 
 <template>
-  <div>Hello world</div>
+  <draggable
+    v-model="model"
+    :component-data="{
+      name: 'fade',
+      type: 'transition-group',
+    }"
+    tag="ul"
+    item-key="value"
+    class="draggable"
+    handle=".handle"
+  >
+    <template #item="{ index }">
+      <DraggableItem v-model="model[index]" />
+    </template>
+  </draggable>
 </template>
+
+<style scoped>
+.draggable {
+  display: flex;
+  flex-direction: column;
+  padding: 16px;
+  border: 1px solid var(--gray-100);
+  border-radius: 4px;
+  background-color: var(--gray-50);
+  gap: 8px;
+}
+</style>
