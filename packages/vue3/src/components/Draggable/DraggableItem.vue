06a15882ec19dbadaf4259d38035448c4071493c
Ali HMIMS <85164582+AliHMIMS@users.noreply.github.com>
2023-02-13 10:17:08 +0100

    Init component

---
+++ packages/vue3/src/components/Draggable/DraggableItem.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import type { DraggableItemType } from './types';
+
+defineProps<{
+  modelValue: DraggableItemType
+}>();
+</script>
+
+<template>
+  <div>
+    <span>Hello</span>
+  </div>
+</template>
---
+++ packages/vue3/src/components/Draggable/Draggables.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import type { DraggableItemType } from './types';
+
+defineProps<{
+  modelValue: DraggableItemType[]
+}>();
+</script>
+
+<template>
+  <div>Hello world</div>
+</template>
