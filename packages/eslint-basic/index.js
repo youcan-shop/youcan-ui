a1a7b551cddfc300694b93cbed1a4b83e12a5212
Ali HMIMS <85164582+AliHMIMS@users.noreply.github.com>
2023-02-23 12:11:21 +0100

    "warn" for console.log()

--- packages/eslint-basic/index.js
+++ packages/eslint-basic/index.js
@@ -219,7 +219,7 @@ module.exports = {
     'comma-dangle': ['error', 'always-multiline'],
     'no-constant-condition': 'warn',
     'no-debugger': 'error',
-    'no-console': ['error', { allow: ['warn', 'error'] }],
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
     'no-cond-assign': ['error', 'always'],
     'func-call-spacing': ['off', 'never'],
     'key-spacing': ['error', { beforeColon: false, afterColon: true }],
