a1b880023a3ea8ebd8f5060196bebf4243b14fcd
Ali HMIMS <85164582+AliHMIMS@users.noreply.github.com>
2023-02-27 11:51:28 +0100

    Bootstrap radio component

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -1,30 +1,17 @@
 <script setup lang="ts">
-import { Sidebar, SidebarItem, SidebarSubitem } from '~/components';
+import Radio from '~/components/Radio/Radio.vue';
 
 import 'uno.css';
 import '../assets/main.css';
 </script>
 
 <template>
-  <Sidebar>
-    <template #header>
-      Store
-    </template>
-    <template #items>
-      <SidebarItem icon="i-youcan-house-simple" label="Home" />
-      <SidebarItem icon="i-youcan-cube" label="Orders">
-        <SidebarSubitem label="All Orders" />
-        <SidebarSubitem label="Create Orders" />
-      </SidebarItem>
-      <SidebarItem icon="i-youcan-tag" label="Products">
-        <SidebarSubitem label="All Products" />
-        <SidebarSubitem label="Create Products" />
-      </SidebarItem>
-      <SidebarItem icon="i-youcan-squares-four" label="Apps" />
-    </template>
-    <template #lower-items>
-      <SidebarItem icon="i-youcan-headset" label="Support" />
-      <SidebarItem icon="i-youcan-gear" label="Settings" />
-    </template>
-  </Sidebar>
+  <div>
+    <Radio name="sssss">
+      weeeee
+    </Radio>
+    <Radio name="sssss">
+      weeeee
+    </Radio>
+  </div>
 </template>
