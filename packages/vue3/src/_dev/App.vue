633436da2e5c11597724eeef57e842b5a2ddbc49
ybenadda <117163420+ybenadda@users.noreply.github.com>
2023-03-02 12:19:03 +0100

    apply the fix

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -1,23 +1,18 @@
 <script setup lang="ts">
 import 'uno.css';
 import '../assets/main.css';
-import { ref } from 'vue';
-import RadioList from '~/components/RadioList/RadioList.vue';
-import type { RadioListOption } from '~/components/RadioList/types';
-
-const options = [
-  { label: 'DHL Express - Morocco', suffix: '35 MAD' },
-  { label: 'Amana Eco - Morocco', suffix: '40 MAD' },
-];
-
-const selected = ref<RadioListOption>();
+import Alert from '~/components/Alert/Alert.vue';
 </script>
 
 <template>
   <div>
     <div class="container">
-      <h3>Selected: {{ selected || 'nothing' }}</h3>
-      <RadioList v-model="selected" :options="options" />
+      <h3>Alert component</h3>
+      <Alert type="info">
+        <template #title>
+          hello
+        </template>
+      </Alert>
     </div>
   </div>
 </template>
