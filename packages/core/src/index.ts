d7fe669d35a8dd5646b11da37ae68035286776f3
Ali HMIMS <85164582+AliHMIMS@users.noreply.github.com>
2023-03-07 11:25:55 +0100

    YSHOP2-134: YouCan-UI > Export types from core package

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,17 @@
 export { default as Utils } from './utils';
 
+/**
+ * Export everything from the utils folder as top level imports
+ * This will replace the usage of `Utils` and `DateUtils`
+ * later on.
+ *
+ * This was also done to ensure types exportage, since
+ * they weren't available when using default import.
+ */
+export * from './utils/type';
+export * from './utils/misc';
+export * from './utils/array';
+
 export * from './color/types';
 export * from './color/helpers';
 
