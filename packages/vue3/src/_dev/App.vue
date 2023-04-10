6a603ead4e95fbb586b3cd3eb505b44f23eb0a2e
Ali HMIMS <85164582+AliHMIMS@users.noreply.github.com>
2023-04-10 15:19:46 +0000

    YSHOP2-299: [Notice bar] for the Mobile NoticeBar, the color picker (#126)

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -1,47 +1,11 @@
 <script setup lang="ts">
 import 'uno.css';
 import '../assets/main.css';
-import { ref } from 'vue';
-import type { TableActions, TableData } from '~/components/Table/types';
-import Table from '~/components/Table/Table.vue';
-
-const data = ref<TableData[]>([{
-  row: {
-    isEditable: true,
-    image: {
-      variant: 'counter',
-      data: {
-        modelValue: '6',
-        min: 4,
-        max: 7,
-      },
-    },
-  },
-  children: [
-    {
-      isEditable: true,
-    },
-    {
-      isEditable: false,
-    },
-  ],
-}]);
-
-const actions = ref<TableActions[]>([
-  {
-    label: 'sss',
-    criteria: (xxx) => {
-      console.log(xxx);
-
-      return true;
-    },
-    iconName: 'i-youcan-x',
-  },
-]);
+import ColorInput from '~/components/Color/ColorInput.vue';
 </script>
 
 <template>
   <div>
-    <Table :actions="actions" :data="data" :columns="[{ accessor: 'image', label: 'input', fullContent: true }, { accessor: 'isEditable', label: 'input', size: '50.0%' }]" />
+    <ColorInput />
   </div>
 </template>
