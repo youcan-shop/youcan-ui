8902cefde8bdbd7217eca14cb4c3c5a05684f3ae
jamyoucn00 <mjamoun@youcan.shop>
2023-06-21 11:30:00 +0100

    DARK: ability to add labelColor in the static status component

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -1,31 +1,18 @@
 <script setup lang="ts">
 import 'uno.css';
 import '../assets/main.css';
-import { ref } from 'vue';
-import ColorInput from '~/components/Color/ColorInput.vue';
+import StaticStatus from '../components/Status/StaticStatus.vue';
 
-const color = ref('#7A7A7A72');
-const alpha = ref(false);
-const showColorInput = ref(true);
-
-const toggleColorInput = () => {
-  showColorInput.value = !showColorInput.value;
-};
 </script>
 
 <template>
   <div>
-    <select id="" v-model="alpha" name="">
-      <option value="true">
-        Preserve transparency
-      </option>
-      <option value="false">
-        Reset transparency
-      </option>
-    </select>
-    <ColorInput v-if="showColorInput" v-model="color" :preserve-transparency="alpha" />
-    <button @click="toggleColorInput">
-      Toggle color picker
-    </button>
+    <StaticStatus
+      :status="{
+        label: 'Test Status',
+        color: '#ff0',
+        labelColor: '#00f',
+      }"
+    />
   </div>
 </template>
--- packages/vue3/src/components/Status/types.ts
+++ packages/vue3/src/components/Status/types.ts
@@ -1,6 +1,7 @@
 export interface StaticStatusDefinition {
   label: string
   color: string
+  labelColor?: string
 }
 
 export interface StatusDefinition<T = any> extends StaticStatusDefinition {
