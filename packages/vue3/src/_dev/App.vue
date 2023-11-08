9a4b6b490336a88073aa674237a4e0304422263f
Mostafa ARABI <maarabi@macs-MacBook-Pro.local>
2023-11-08 16:15:00 +0100

    fix z-index color picker issue

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -1,57 +1,21 @@
 <script setup lang="ts">
-import 'uno.css';
-import '../assets/main.css';
 import { ref } from 'vue';
-import DropDown from '~/components/Dropdown/Dropdown.vue';
+import { Tag } from '~/components';
+import type { TagItemValue } from '~/components/Tag/types';
 
-const items = [
-  { label: 'Shoes', value: 1 },
-  { label: 'Cosmetics', value: 2 },
-  { label: 'Gym', value: 3 },
-  { label: 'test', value: 4 },
-  { label: 'something', value: 5 },
-  { label: 'hi', value: 6 },
-];
-
-const category = ref(null);
-
-function search(term: string) {
-  console.log('soso');
-  const data = [
-    {
-      label: term,
-      value: 1,
-    },
-  ];
-
-  for (let index = 0; index < term.length; index++) {
-    items.push({
-      label: String(Math.floor(Math.random() * 1000)),
-      value: String(Math.floor(Math.random() * 1000)),
-    });
-  }
-
-  // if we return array here, it will act like the dropdown items, but if we return nothing the dropdown will just use the items prop
-  // return [
-  //   {
-  //     label: term,
-  //     value: 1,
-  //   },
-  // ];
-}
+const preferredLanguages = ref<TagItemValue[]>([
+  { label: 'Pink', hexColor: '#F49FBC' },
+  { label: 'Lavender', hexColor: '#805D93' },
+  { label: 'Apricot', hexColor: '#FFD3BA' },
+  { label: 'Olive Green', hexColor: '#9EBD6E' },
+  { label: 'Shamrock Green', hexColor: '#169873' },
+]);
 </script>
 
 <template>
-  <div>
-    <div style="padding: 5rem;background: red;">
-      {{ search }}
-    </div>
-    <DropDown
-      v-model="category"
-      searchable
-      :items="items"
-      placeholder="Select category"
-      :search-handler="search"
-    />
-  </div>
+  <Tag
+    v-model="preferredLanguages"
+    placeholder="Your favorite colors"
+    type="color"
+  />
 </template>
