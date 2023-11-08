2a8a7f3043c93cac3cf83305e65c96497077b210
Mostafa ARABI <maarabi@macs-MacBook-Pro.local>
2023-11-08 12:20:41 +0100

    add object fit to Thumbnail component

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -2,56 +2,11 @@
 import 'uno.css';
 import '../assets/main.css';
 import { ref } from 'vue';
-import DropDown from '~/components/Dropdown/Dropdown.vue';
+import { Thumbnail } from '~/components';
 
-const items = [
-  { label: 'Shoes', value: 1 },
-  { label: 'Cosmetics', value: 2 },
-  { label: 'Gym', value: 3 },
-  { label: 'test', value: 4 },
-  { label: 'something', value: 5 },
-  { label: 'hi', value: 6 },
-];
-
-const category = ref(null);
-
-function search(term: string) {
-  console.log('soso');
-  const data = [
-    {
-      label: term,
-      value: 1,
-    },
-  ];
-
-  for (let index = 0; index < term.length; index++) {
-    items.push({
-      label: String(Math.floor(Math.random() * 1000)),
-      value: String(Math.floor(Math.random() * 1000)),
-    });
-  }
-
-  // if we return array here, it will act like the dropdown items, but if we return nothing the dropdown will just use the items prop
-  // return [
-  //   {
-  //     label: term,
-  //     value: 1,
-  //   },
-  // ];
-}
+const src = ref('');
 </script>
 
 <template>
-  <div>
-    <div style="padding: 5rem;background: red;">
-      {{ search }}
-    </div>
-    <DropDown
-      v-model="category"
-      searchable
-      :items="items"
-      placeholder="Select category"
-      :search-handler="search"
-    />
-  </div>
+  <Thumbnail :src="src" />
 </template>
