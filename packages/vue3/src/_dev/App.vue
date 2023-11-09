1cb9e1c27dcb64fe9f6fe754ed2b4cb6969ba118
mosarabi <149577299+mosarabi@users.noreply.github.com>
2023-11-09 14:48:11 +0100

    fix z-index color picker issue (#148)
    Co-authored-by: Mostafa ARABI <maarabi@macs-MacBook-Pro.local>

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -2,11 +2,22 @@
 import 'uno.css';
 import '../assets/main.css';
 import { ref } from 'vue';
-import { Thumbnail } from '~/components';
+import { Tag } from '~/components';
+import type { TagItemValue } from '~/components/Tag/types';
 
-const src = ref('');
+const preferredLanguages = ref<TagItemValue[]>([
+  { label: 'Pink', hexColor: '#F49FBC' },
+  { label: 'Lavender', hexColor: '#805D93' },
+  { label: 'Apricot', hexColor: '#FFD3BA' },
+  { label: 'Olive Green', hexColor: '#9EBD6E' },
+  { label: 'Shamrock Green', hexColor: '#169873' },
+]);
 </script>
 
 <template>
-  <Thumbnail :src="src" />
+  <Tag
+    v-model="preferredLanguages"
+    placeholder="Your favorite colors"
+    type="color"
+  />
 </template>
