6d628ada99d2282b05b63b3cd5f82e26ac711c84
mosarabi <149577299+mosarabi@users.noreply.github.com>
2023-11-09 14:27:53 +0100

    add uno.css to .Appvue

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import 'uno.css';
+import '../assets/main.css';
 import { ref } from 'vue';
 import { Tag } from '~/components';
 import type { TagItemValue } from '~/components/Tag/types';
