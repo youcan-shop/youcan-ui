39eaccb7d6ee0aaa372c23e0477e78f14bf51a6a
mosarabi <149577299+mosarabi@users.noreply.github.com>
2023-11-13 11:38:08 +0100

    remove the action tooltip when criteria is false (#152)

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -2,51 +2,145 @@
 import 'uno.css';
 import '../assets/main.css';
 import { ref } from 'vue';
-import { EditableStatus } from '~/components';
-import type { StatusDefinition } from '~/components/Status/types';
+import { Table } from '~/components';
+import type { StaticStatusDefinition } from '~/components/Status/types';
+import type { TableActions, TableColumn, TableData } from '~/components/Table/types';
 
-const fruits: StatusDefinition[] = [
+const columns: TableColumn[] = [
+  { label: 'Domain', accessor: 'domain', sortable: 'desc' },
+  { label: 'Status', accessor: 'status', sortable: 'desc' },
+  { label: 'Git Branch', accessor: 'branch', sortable: 'desc' },
+];
+
+const actions: TableActions[] = [
   {
-    color: '#ffdecb',
-    label: 'Peach 🍑',
-    labelColor: 'red',
-    value: 'peach',
+    label: 'Redeploy',
+    iconName: 'i-youcan-arrows-clockwise',
+    tooltip: 'Redeploy',
+    criteria: () => true,
   },
   {
-    color: '#fffad2',
-    label: 'Banana 🍌',
-    labelColor: 'orange',
-    value: 'banana',
+    label: 'Copy URL',
+    iconName: 'i-youcan-copy',
+    tooltip: 'Copy URL',
+    criteria: () => false,
   },
   {
-    color: '#cbffd3',
-    label: 'Kiwi 🥝',
-    labelColor: 'green',
-    value: 'kiwi',
+    label: 'Source Code',
+    iconName: 'i-youcan-code',
+    tooltip: 'Source Code',
+    criteria: () => true,
   },
 ];
-const favoriteFruit = ref(fruits[0]);
+
+const domainStatuses: { [k: string]: StaticStatusDefinition } = {
+  ready: {
+    color: '#52E2C0',
+    label: 'Ready',
+    labelColor: '#2d4236',
+  },
+  error: {
+    color: '#EE0200',
+    label: 'Error',
+    labelColor: '#f8dcdc',
+  },
+  building: {
+    color: '#ffc35c',
+    label: 'Building',
+    labelColor: '#574811',
+  },
+  queued: {
+    color: '#999999',
+    label: 'Queued',
+    labelColor: '#fff2f2',
+  },
+};
+
+const data = ref<TableData[]>([
+  {
+    row: {
+      id: 'eihabkhan.com',
+      domain: 'eihabkhan.com',
+      status: {
+        variant: 'static-status',
+        data: {
+          status: domainStatuses.building,
+        },
+      },
+      branch: 'main',
+    },
+  },
+  {
+    row: {
+      id: 'supershop.youcan.shop',
+      domain: 'supershop.youcan.shop',
+      status: {
+        variant: 'static-status',
+        data: {
+          status: domainStatuses.queued,
+        },
+      },
+      branch: 'main',
+    },
+  },
+  {
+    row: {
+      id: 'vercel.com',
+      domain: 'vercel.com',
+      status: {
+        variant: 'static-status',
+        data: {
+          status: domainStatuses.ready,
+        },
+      },
+      branch: 'main',
+    },
+  },
+  {
+    row: {
+      domain: 'nextjs.org',
+      status: {
+        variant: 'static-status',
+        data: {
+          status: domainStatuses.error,
+        },
+      },
+      branch: 'feat/next-conf',
+    },
+  },
+]);
+
+const selectedRows = ref<TableData[]>([]);
+
+const handleSort = (column: TableColumn) => {
+  data.value = data.value.sort((a, b) => {
+    const textA = (a.row[column.accessor] as string).toUpperCase();
+    const textB = (b.row[column.accessor] as string).toUpperCase();
+
+    if (textA < textB) {
+      return -1;
+    }
+    if (textA > textB) {
+      return 1;
+    }
+
+    return 0;
+  });
+};
+
+const updateSelectedRows = (data: TableData[]) => {
+  selectedRows.value = data;
+};
 </script>
 
 <template>
-  <EditableStatus v-model="favoriteFruit" :statuses="fruits" />
+  <Table
+    :columns="columns"
+    :data="data"
+    :actions="actions"
+    :selectable="true"
+    :selected-rows="selectedRows"
+    @sort="handleSort"
+    @update:selected-rows="updateSelectedRows"
+  />
 </template>
-
-<style scoped>
-.container {
-  display: flex;
-  position: relative;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 300px;
-  min-height: 140px;
-}
-
-.file-input {
-  position: absolute;
-  right: 0;
-  bottom: 0;
-  left: 0;
-}
-</style>
