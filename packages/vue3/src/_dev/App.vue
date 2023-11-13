996572a4fdf94efb2cffa6745a983270f85cedc9
Eihab Khan <143792300+eihabkhan1@users.noreply.github.com>
2023-11-13 11:04:05 +0100

    fix dropdown checkboxes emitting check twice

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -2,34 +2,25 @@
 import 'uno.css';
 import '../assets/main.css';
 import { ref } from 'vue';
-import { EditableStatus } from '~/components';
-import type { StatusDefinition } from '~/components/Status/types';
+import { MultiselectDropdown } from '~/components';
 
-const fruits: StatusDefinition[] = [
-  {
-    color: '#ffdecb',
-    label: 'Peach 🍑',
-    labelColor: 'red',
-    value: 'peach',
-  },
-  {
-    color: '#fffad2',
-    label: 'Banana 🍌',
-    labelColor: 'orange',
-    value: 'banana',
-  },
-  {
-    color: '#cbffd3',
-    label: 'Kiwi 🥝',
-    labelColor: 'green',
-    value: 'kiwi',
-  },
+const desiredLanguages = ref([]);
+
+const languages = [
+  { label: 'Arabic', value: 1 },
+  { label: 'English', value: 2 },
+  { label: 'German', value: 3 },
 ];
-const favoriteFruit = ref(fruits[0]);
 </script>
 
 <template>
-  <EditableStatus v-model="favoriteFruit" :statuses="fruits" />
+  <MultiselectDropdown
+    v-model="desiredLanguages"
+    :searchable="true"
+    :items="languages"
+    label="Desired languages"
+    icon="i-youcan-translate"
+  />
 </template>
 
 <style scoped>
