8e14fbe4b834686d69ecedb17d5f731b3225d6c2
mosarabi <149577299+mosarabi@users.noreply.github.com>
2023-11-22 17:17:46 +0100

    Create Modal component

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -2,39 +2,43 @@
 import 'uno.css';
 import '../assets/main.css';
 import { ref } from 'vue';
-import { Dropdown } from '~/components';
-const category = ref(null);
+import Modal from '~/components/Modal/Modal.vue';
+import { PrimaryButton } from '~/components';
+const showModal = ref(false);
 
-const items = [
-  { label: 'Shoes', value: 1 },
-  { label: 'Cosmetics', value: 2 },
-  { label: 'Gym', value: 3 },
-];
+const onConfirm = () => {
+  /*
+  * your code here
+  */
+};
 </script>
 
 <template>
+  <Modal v-model:visible="showModal" @on-confirm="onConfirm">
+    <p>
+      Lorem ipsum dolor sit,
+      Lorem ipsum dolor sit,
+      Lorem ipsum dolor sit,
+    </p>
+  </Modal>
   <div class="container">
-    <Dropdown
-      v-model="category"
-      searchable
-      :items="items"
-      placeholder="Select category"
-    />
-  </div>
-
-  <div class="container" style="margin-top: calc(100vh - 200px);">
-    <Dropdown
-      v-model="category"
-      searchable
-      :items="items"
-      placeholder="Select category"
-    />
+    <PrimaryButton @click="showModal = true;">
+      <span>Show Modal {{ showModal }}</span>
+    </PrimaryButton>
   </div>
 </template>
 
 <style scoped>
 .container {
-  max-width: 400px;
-  margin: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+p {
+  margin-bottom: 0;
+  color: var(--gray-600);
+  font: var(--text-sm-regular);
 }
 </style>
