f30a39cafd5e7d577bfbf8ca020727ff27eba155
mosarabi <149577299+mosarabi@users.noreply.github.com>
2023-11-24 12:20:52 +0100

    YSHOP2-1060: Create Modal component (#163)

---
+++ packages/vue3/src/components/Overlay/Overlay.vue
@@ -0,0 +1,55 @@
+<script lang="ts" setup>
+const emit = defineEmits(['onBackdropClick']);
+</script>
+
+<template>
+  <div class="overlay">
+    <div class="backdrop" @click="emit('onBackdropClick')" />
+    <div class="body">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.overlay {
+  display: flex;
+  position: fixed;
+  z-index: 999999999;
+  top: 0;
+  left: 0;
+  align-items: center;
+  width: 100vw;
+  min-width: 100vw;
+  max-width: 100vw;
+  height: 100vh;
+  min-height: 100vh;
+  max-height: 100vh;
+
+  .backdrop,
+  &::before {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    cursor: pointer;
+  }
+
+  &::before {
+    content: "";
+    z-index: 1;
+    opacity: 0.24;
+    background-color: var(--gray-900);
+  }
+
+  .backdrop {
+    z-index: 2;
+  }
+
+  .body {
+    position: relative;
+    z-index: 3;
+    max-width: 100%;
+    margin: auto;
+  }
+}
+</style>
