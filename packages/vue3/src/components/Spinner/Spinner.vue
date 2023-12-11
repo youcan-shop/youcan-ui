9c5bedd48994f7cdf36a173285b864e35a9183fd
Ghita Nekt <ghitanekt@admins-MacBook-Pro.local>
2023-12-11 15:06:05 +0100

    Added a spinner component with a label

---
+++ packages/vue3/src/components/Spinner/Spinner.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+import type { SpinnerType } from './types';
+
+defineProps<{ spinner: SpinnerType }>();
+</script>
+
+<template>
+  <div class="container">
+    <div class="spinner" />
+    <p class="label">
+      {{ spinner.label }}
+    </p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+}
+.label {
+  font: v-bind(
+    "`var(--text-${spinner.labelFontSize}-regular)` || 'var(--text-md-regular)'"
+  );
+  color: v-bind("spinner.labelColor || 'var(--brand-600)'");
+}
+.spinner {
+  width: v-bind("spinner.size || '50px'");
+  aspect-ratio: 1;
+  border-radius: 50%;
+  border: 8px solid #0000;
+  border-right-color: v-bind("spinner.color || 'var(--brand-600)'");
+  position: relative;
+  animation: s4 1s infinite linear;
+}
+.spinner:before,
+.spinner:after {
+  content: "";
+  position: absolute;
+  inset: -8px;
+  border-radius: 50%;
+  border: inherit;
+  animation: inherit;
+  animation-duration: 2s;
+}
+.spinner:after {
+  animation-duration: 4s;
+}
+
+@keyframes s4 {
+  100% {
+    transform: rotate(1turn);
+  }
+}
+</style>
