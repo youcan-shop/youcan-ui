0372b477a92fbf98ea1362b471946cd48384cf66
Eihab Khan <143792300+eihabkhan1@users.noreply.github.com>
2023-12-13 17:02:42 +0100

    YSHOP2-1122: Add hide-per-page prop to PaginationBar (#180)

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -1,13 +1,31 @@
 <script setup lang="ts">
 import 'uno.css';
 import '../assets/main.css';
-import { Divider } from '~/components';
+import { ref } from 'vue';
+
+import { PaginationBar } from '~/components';
+
+const TOTAL = 50;
+const PER_PAGE = 5;
+const TOTAL_PAGES = Math.ceil(TOTAL / PER_PAGE);
+
+const currentPage = ref(1);
+
+function handlePaginationNavigation(pageNumber: number) {
+  currentPage.value = pageNumber;
+}
 </script>
 
 <template>
-  <div class="container">
-    <Divider />
-  </div>
+  <PaginationBar
+    :hide-per-page="false"
+    :current="currentPage"
+    :size="TOTAL_PAGES"
+    :count="PER_PAGE"
+    :total="TOTAL"
+    per-page-label="Showing :count of :total results"
+    @update:current="handlePaginationNavigation"
+  />
 </template>
 
 <style scoped>
