dac9ce5ca5e24133d0dad7dd9284381f5078f201
GhitaNektt <152861043+GhitaNektt@users.noreply.github.com>
2023-12-13 17:33:00 +0100

    YSHOP2-1128: Added Skeleton component (#182)
    Co-authored-by: Ghita Nekt <ghitanekt@admins-MacBook-Pro.local>

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -2,36 +2,21 @@
 import 'uno.css';
 import '../assets/main.css';
 import { ref } from 'vue';
+import { Skeleton } from '~/components';
 
-import { PaginationBar } from '~/components';
-
-const TOTAL = 50;
-const PER_PAGE = 5;
-const TOTAL_PAGES = Math.ceil(TOTAL / PER_PAGE);
-
-const currentPage = ref(1);
-
-function handlePaginationNavigation(pageNumber: number) {
-  currentPage.value = pageNumber;
-}
+const lines = ref(10);
 </script>
 
 <template>
-  <PaginationBar
-    :hide-per-page="false"
-    :current="currentPage"
-    :size="TOTAL_PAGES"
-    :count="PER_PAGE"
-    :total="TOTAL"
-    per-page-label="Showing :count of :total results"
-    @update:current="handlePaginationNavigation"
-  />
+  <div class="container">
+    <Skeleton type="text" :lines="lines" />
+    <Skeleton type="media" />
+  </div>
 </template>
 
 <style scoped>
 .container {
   display: flex;
-  flex-direction: column;
   align-items: center;
   justify-content: center;
   height: 90vh;
--- packages/vue3/src/components/index.ts
+++ packages/vue3/src/components/index.ts
@@ -55,3 +55,4 @@ export { default as Tooltip } from './Tooltip/Tooltip.vue';
 export { default as Slider } from './Slider/Slider.vue';
 export { default as Modal } from './Modal/Modal.vue';
 export { default as Divider } from './Divider/Divider.vue';
+export { default as Skeleton } from './Skeleton/Skeleton.vue';
