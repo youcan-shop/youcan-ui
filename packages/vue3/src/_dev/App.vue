1887ea610fb5534ed1cb29c12fb89dece2474b5b
mosarabi <149577299+mosarabi@users.noreply.github.com>
2023-12-26 16:11:00 +0100

    Feature Callout > Add edge awareness

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -1,134 +1,39 @@
 <script setup lang="ts">
 import 'uno.css';
-import '~/assets/main.css';
+import '../assets/main.css';
 import { ref } from 'vue';
-import { PrimaryButton, ResourcePicker } from '~/components';
-import type { Resource } from '~/components/ResourcePicker/types';
-
-const MOCK_RESOURCES: Resource[] = [
-  {
-    id: 1,
-    thumbnailUrl: '',
-    name: 'Apple MacBook Pro',
-    price: '$10.99',
-    stock: 99,
-    isChecked: false,
-    variants: [
-      {
-        id: 33,
-        thumbnailUrl: '',
-        name: 'Apple MacBook Pro 16 M3 Max',
-        price: '$10.99',
-        stock: 99,
-        isChecked: false,
-      },
-      {
-        id: 21,
-        thumbnailUrl: '',
-        name: 'Apple MacBook Pro 14 M3 Pro',
-        price: '$10.99',
-        stock: 99,
-        isChecked: false,
-      },
-    ],
-  },
-  {
-    id: 2,
-    thumbnailUrl: '',
-    name: 'Apple iMac',
-    price: '$10.99',
-    stock: 99,
-    isChecked: false,
-  },
-  {
-    id: 3,
-    thumbnailUrl: '',
-    name: 'Apple Watch',
-    price: '$10.99',
-    stock: 99,
-    isChecked: false,
-  },
-];
-
-const showPicker = ref(false);
-const showLoadingPicker = ref(false);
-const showEmptyPicker = ref(false);
-const selectedResources = ref<Resource[]>([]);
-
-const onConfirm = (resources: Resource[]) => {
-  selectedResources.value = resources;
-  showPicker.value = false;
-};
-
-const onSearch = (term: string) => {
-  console.log('Search term:', term);
-};
+import { Popover, PrimaryButton } from '~/components';
+const show = ref(false);
 </script>
 
 <template>
   <div class="container">
-    <div class="picker">
-      <ResourcePicker
-        v-model:visible="showPicker"
-        :resources="MOCK_RESOURCES"
-        stock-label="in stock"
-        :is-loading="false"
-        @confirm="onConfirm"
-        @search="onSearch"
-      />
-      <PrimaryButton @click="showPicker = true;">
-        <span>Open Picker</span>
+    <Popover
+      v-model:show="show"
+      position="right"
+      object-fit="contain"
+    >
+      <template #title>
+        Introducing Themes!
+      </template>
+      <template #description>
+        <p>
+          Introducing themes, a new way to make yourstore stand out and make it
+          more attractive towards your existing and potential clients.
+        </p>
+      </template>
+      <PrimaryButton @click="show = !show;">
+        <span>Show Popover</span>
       </PrimaryButton>
-    </div>
-    <div class="picker">
-      <ResourcePicker
-        v-model:visible="showLoadingPicker"
-        :is-loading="true"
-      />
-      <PrimaryButton @click="showLoadingPicker = true;">
-        <span>Open Loading Picker</span>
-      </PrimaryButton>
-    </div>
-    <div class="picker">
-      <ResourcePicker
-        v-model:visible="showEmptyPicker"
-        :resources="[]"
-        :is-loading="false"
-      />
-      <PrimaryButton @click="showEmptyPicker = true;">
-        <span>Open Empty Picker</span>
-      </PrimaryButton>
-    </div>
-    <div class="selection-container">
-      <p>Selected Resources:</p>
-      <pre>{{ selectedResources }}</pre>
-    </div>
+    </Popover>
   </div>
 </template>
 
 <style scoped>
 .container {
   display: flex;
-  flex-direction: column;
   align-items: center;
   justify-content: center;
-  width: 100vw;
-  min-height: 100vh;
-  margin: 0 auto;
-  padding: 32px 0;
-  /* stylelint-disable-next-line font-family-no-missing-generic-family-keyword */
-  font-family: "Mona Sans";
-  gap: 32px;
-}
-
-.selection-container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  min-width: 400px;
-  padding: 32px;
-  border: 1px solid var(--gray-200);
-  border-radius: 8px;
+  height: 100vh;
 }
 </style>
--- packages/vue3/src/components/Popover/Popover.vue
+++ packages/vue3/src/components/Popover/Popover.vue
@@ -1,6 +1,7 @@
 <script lang="ts" setup>
 import { onClickOutside } from '@vueuse/core';
-import { ref, useSlots } from 'vue';
+import { onMounted, onUnmounted, ref, useSlots } from 'vue';
+import { setPosition } from '../Tooltip/utils';
 import type { ObjectFit } from './types';
 const props = withDefaults(defineProps<{
   position?: 'left' | 'right' | 'top' | 'bottom'
@@ -11,32 +12,51 @@ const props = withDefaults(defineProps<{
   position: 'left',
   objectFit: 'cover',
 });
-const emit = defineEmits(['clickOutside']);
+const emit = defineEmits(['update:show']);
 
 const popover = ref();
+const element = ref();
+const popoverTrigger = ref();
+const top = ref('0px');
+const left = ref('0px');
+const currentPosition = ref('');
 
 const slots = useSlots();
 
-const transitionName = () => {
-  switch (props.position) {
-    case 'left':
-    case 'right':
-      return 'fade-hr';
-
-    default:
-      return 'fade-vr';
+const setPopoverPosition = () => {
+  if (popoverTrigger.value && element.value) {
+    const position = setPosition(popoverTrigger.value, element.value, props.position, 22);
+    top.value = `${position.top}px`;
+    left.value = `${position.left}px`;
+    currentPosition.value = position.currentPosition;
   }
 };
 
 onClickOutside(popover, () => {
-  emit('clickOutside');
+  emit('update:show', false);
+});
+
+const handleClick = () => {
+  setPopoverPosition();
+};
+
+const handleScroll = () => {
+  emit('update:show', false);
+};
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('scroll', handleScroll);
 });
 </script>
 
 <template>
   <div ref="popover" class="popover">
-    <Transition :name="transitionName()">
-      <div v-if="show" class="popover-trigger" :class="position">
+    <Transition name="fade">
+      <div v-if="show" ref="popoverTrigger" class="popover-trigger" :class="[currentPosition, { 'has-footer': slots.footer }]">
         <div class="content">
           <div v-if="imgSrc" class="image">
             <img :src="imgSrc">
@@ -53,7 +73,9 @@ onClickOutside(popover, () => {
         </div>
       </div>
     </Transition>
-    <slot />
+    <div ref="element" @click="handleClick">
+      <slot />
+    </div>
   </div>
 </template>
 
@@ -67,7 +89,10 @@ $caret-border: 1px solid var(--gray-300);
 
   &-trigger {
     display: flex;
-    position: absolute;
+    position: fixed;
+    z-index: 9999999999;
+    top: v-bind(top);
+    left: v-bind(left);
     flex-direction: column;
     width: 400px;
     max-width: calc(100vw - 40px);
@@ -138,16 +163,12 @@ $caret-border: 1px solid var(--gray-300);
 
     &.left,
     &.right {
-      top: -12px;
-
       &::after {
-        top: 24px;
+        top: calc(50% - calc($caret-size / 2));
       }
     }
 
     &.left {
-      right: calc(100% + $margin);
-
       &::after {
         left: calc(100% - calc($caret-size / 2));
         transform: rotate(45deg);
@@ -155,8 +176,6 @@ $caret-border: 1px solid var(--gray-300);
     }
 
     &.right {
-      left: calc(100% + $margin);
-
       &::after {
         right: calc(100% - calc($caret-size / 2));
         transform: rotate(-135deg);
@@ -165,27 +184,25 @@ $caret-border: 1px solid var(--gray-300);
 
     &.top,
     &.bottom {
-      left: 50%;
-      transform: translateX(-50%);
-
       &::after {
         left: calc(50% - calc($caret-size / 2));
       }
     }
 
     &.top {
-      bottom: calc(100% + $margin);
-
       &::after {
         top: calc(100% - calc($caret-size / 2));
         transform: rotate(135deg);
-        background-color: var(--gray-50);
+      }
+
+      &.has-footer {
+        &::after {
+          background-color: var(--gray-50);
+        }
       }
     }
 
     &.bottom {
-      top: calc(100% + $margin);
-
       &::after {
         bottom: calc(100% - calc($caret-size / 2));
         transform: rotate(-45deg);
@@ -194,15 +211,15 @@ $caret-border: 1px solid var(--gray-300);
   }
 }
 
-.fade-hr-enter-active {
-  animation: fade-hr 0.25s ease-in-out;
+.fade-enter-active {
+  animation: fade 0.25s ease-in-out;
 }
 
-.fade-hr-leave-active {
-  animation: fade-hr 0.25s reverse ease-in-out;
+.fade-leave-active {
+  animation: fade 0.25s reverse ease-in-out;
 }
 
-@keyframes fade-hr {
+@keyframes fade {
   0% {
     transform: scale(0.98);
     opacity: 0;
@@ -210,25 +227,6 @@ $caret-border: 1px solid var(--gray-300);
 
   100% {
     transform: scale(1);
-    opacity: 1;
-  }
-}
-
-.fade-vr-enter-active {
-  animation: fade-vr 0.25s ease-in-out;
-}
-
-.fade-vr-leave-active {
-  animation: fade-vr 0.25s reverse ease-in-out;
-}
-
-@keyframes fade-vr {
-  0% {
-    transform: scale(0.98) translateX(-50%);
-    opacity: 0;
-  }
-
-  100% {
     opacity: 1;
   }
 }
