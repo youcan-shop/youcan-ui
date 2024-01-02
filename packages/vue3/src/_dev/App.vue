fc7bf024867d7a24bda3f93abdebe1f0dcdf74d9
mosarabi <149577299+mosarabi@users.noreply.github.com>
2024-01-02 14:42:58 +0100

    YSHOP2-1217: Add fixes based on Feedback meeting (#203)

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -1,190 +1,109 @@
 <script setup lang="ts">
 import 'uno.css';
 import '../assets/main.css';
-import { computed, ref } from 'vue';
-import { PaginationBar, Table } from '~/components';
+import { ref } from 'vue';
 import type { StaticStatusDefinition } from '~/components/Status/types';
-import type { TableActions, TableColumn, TableData } from '~/components/Table/types';
+import { Dropdown, MultiselectDropdown, StaticStatus } from '~/components';
 
-const columns: TableColumn[] = [
-  { label: 'Domain', accessor: 'domain', sortable: 'none' },
-  { label: 'Status', accessor: 'status', sortable: 'desc' },
-  { label: 'Git Branch', accessor: 'branch', sortable: 'desc' },
+const category = ref(null);
+const desiredLanguages = ref([]);
+
+const nameList = [
+  'Time', 'Past', 'Future', 'Dev',
+  'Fly', 'Flying', 'Soar', 'Soaring', 'Power', 'Falling',
+  'Fall', 'Jump', 'Cliff', 'Mountain', 'Rend', 'Red', 'Blue',
+  'Green', 'Yellow', 'Gold', 'Demon'];
+
+const fruits: StaticStatusDefinition[] = [
+  {
+    color: '#ffdecb',
+    label: '',
+    labelColor: '#35192b',
+  },
+  {
+    color: '#fffad2',
+    label: '',
+    labelColor: '#555022',
+  },
+  {
+    color: '#cbffd3',
+    label: '',
+    labelColor: '#2c4730',
+  },
+  {
+    color: '#EEAAAA',
+    label: '',
+    labelColor: '#7B1919',
+  },
 ];
 
-const actions: TableActions[] = [
-  {
-    label: 'Redeploy',
-    iconName: 'i-youcan-arrows-clockwise',
-    tooltip: 'Redeploy',
-  },
-  {
-    label: 'Copy URL',
-    iconName: 'i-youcan-copy',
-    tooltip: 'Copy URL',
-  },
-  {
-    label: 'Source Code',
-    iconName: 'i-youcan-code',
-    tooltip: 'Source Code',
-  },
-];
+const getStatus = (text: string): StaticStatusDefinition => {
+  const status = fruits[Math.floor(Math.random() * (fruits.length))];
+  status.label = text;
 
-const domainStatuses: { [k: string]: StaticStatusDefinition } = {
-  ready: {
-    color: '#52E2C0',
-    label: 'Ready',
-    labelColor: '#2d4236',
-  },
-  error: {
-    color: '#EE0200',
-    label: 'Error',
-    labelColor: '#f8dcdc',
-  },
-  building: {
-    color: '#ffc35c',
-    label: 'Building',
-    labelColor: '#574811',
-  },
-  queued: {
-    color: '#999999',
-    label: 'Queued',
-    labelColor: '#fff2f2',
-  },
+  return status;
 };
-
-const data = ref<TableData[]>([
-  {
-    row: {
-      id: 'eihabkhan.com',
-      domain: 'eihabkhan.com',
-      status: {
-        variant: 'static-status',
-        data: {
-          status: domainStatuses.building,
-        },
-      },
-      branch: 'main',
-    },
-  },
-  {
-    row: {
-      id: 'supershop.youcan.shop',
-      domain: 'supershop.youcan.shop',
-      status: {
-        variant: 'static-status',
-        data: {
-          status: domainStatuses.queued,
-        },
-      },
-      branch: 'main',
-    },
-  },
-  {
-    row: {
-      id: 'vercel.com',
-      domain: 'vercel.com',
-      status: {
-        variant: 'static-status',
-        data: {
-          status: domainStatuses.ready,
-        },
-      },
-      branch: 'main',
-    },
-  },
-  {
-    row: {
-      domain: 'nextjs.org',
-      status: {
-        variant: 'static-status',
-        data: {
-          status: domainStatuses.error,
-        },
-      },
-      branch: 'feat/next-conf',
-    },
-  },
-]);
-
-const selectedRows = ref<TableData[]>([]);
-
-const handleSort = (column: TableColumn) => {
-  data.value = data.value.sort((a: any, b: any) => {
-    let textA = '';
-    let textB = '';
-
-    if (typeof a.row[column.accessor] === 'string') {
-      textA = (a.row[column.accessor] as string)?.toUpperCase();
-      textB = (b.row[column.accessor] as string)?.toUpperCase();
-    }
-
-    else if (a.row?.status?.data.status.label && b.row?.status?.data.status.label) {
-      textA = a.row?.status?.data.status.label;
-      textB = b.row?.status?.data.status.label;
-    }
-
-    if (textA < textB) {
-      return -1;
-    }
-    if (textA > textB) {
-      return 1;
-    }
-
-    return 0;
-  });
-};
-
-const updateSelectedRows = (data: TableData[]) => {
-  selectedRows.value = data;
-};
-
-const TOTAL = 50;
-const PER_PAGE = 5;
-const TOTAL_PAGES = Math.ceil(TOTAL / PER_PAGE);
-
-const currentPage = ref(2);
-
-const count = computed(() => {
-  const page = currentPage.value - 1;
-
-  return `${(PER_PAGE * page) + 1} - ${(PER_PAGE * page) + PER_PAGE}`;
+const items = Array.from({ length: 100 }, () => {
+  return {
+    label: nameList[Math.floor(Math.random() * (nameList.length - 1))],
+    value: Math.floor(Math.random() * 10000000),
+  };
 });
 
-function handlePaginationNavigation(pageNumber: number) {
-  currentPage.value = pageNumber;
-}
+const languages = [
+  { label: 'Arabic', value: 1 },
+  { label: 'English', value: 2 },
+  { label: 'German', value: 3 },
+  { label: 'Arabic', value: 4 },
+  { label: 'English', value: 5 },
+  { label: 'German', value: 6 },
+  { label: 'Arabic', value: 7 },
+  { label: 'English', value: 8 },
+  { label: 'German', value: 9 },
+];
 </script>
 
 <template>
-  <div class="container">
-    <Table
-      :columns="columns"
-      :data="data"
-      :actions="actions"
-      actions-text="My actions"
-      :selectable="true"
-      :selected-rows="selectedRows"
-      @sort="handleSort"
-      @update:selected-rows="updateSelectedRows"
+  <div class="dropdown-container">
+    <Dropdown
+      v-model="category"
+      searchable
+      :items="items"
+      placeholder="Select category"
     >
-      <template #domain="row">
-        <div> {{ row.row.domain.value }} | {{ row.row.branch.value }}</div>
+      <template #accessory="item">
+        <div class="status">
+          <StaticStatus :status="getStatus(item.label)" />
+        </div>
       </template>
-    </Table>
-    <PaginationBar
-      :current="currentPage"
-      :size="TOTAL_PAGES"
-      :count="count"
-      :total="TOTAL"
-      @update:current="handlePaginationNavigation"
-    />
+    </Dropdown>
+    <MultiselectDropdown
+      v-model="desiredLanguages"
+      :items="languages"
+      label="Desired languages"
+      icon="i-youcan-translate"
+    >
+      <template #accessory="item">
+        <div class="status">
+          {{ item.value }}
+        </div>
+      </template>
+    </MultiselectDropdown>
   </div>
 </template>
 
 <style scoped>
-.container {
-  max-width: 60%;
+.dropdown-container {
+  display: flex;
+  flex-direction: column;
+  width: 300px;
   margin: 60px auto;
+  row-gap: 60px;
+}
+
+.status {
+  display: flex;
+  flex: 1;
+  justify-content: end;
 }
 </style>
