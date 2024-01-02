4a579e98c18e193ea123c9f84642dc7a21814dd9
mosarabi <149577299+mosarabi@users.noreply.github.com>
2024-01-02 11:35:12 +0100

    YSHOP2-1218: Table > Add fixes based on Feedback meeting (#205)

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -1,106 +1,190 @@
 <script setup lang="ts">
 import 'uno.css';
 import '../assets/main.css';
-import { ref } from 'vue';
-import { PrimaryButton, ResourcePicker } from '~/components';
-import type { Resource } from '~/components/ResourcePicker/types';
+import { computed, ref } from 'vue';
+import { PaginationBar, Table } from '~/components';
+import type { StaticStatusDefinition } from '~/components/Status/types';
+import type { TableActions, TableColumn, TableData } from '~/components/Table/types';
 
-const MOCK_RESOURCES: Resource[] = [
+const columns: TableColumn[] = [
+  { label: 'Domain', accessor: 'domain', sortable: 'none' },
+  { label: 'Status', accessor: 'status', sortable: 'desc' },
+  { label: 'Git Branch', accessor: 'branch', sortable: 'desc' },
+];
+
+const actions: TableActions[] = [
   {
-    id: 1,
-    thumbnailUrl: '',
-    name: 'Apple MacBook Pro',
-    price: '$2,499.00',
-    stock: 7,
-    isChecked: false,
-    variants: [
-      {
-        id: 33,
-        thumbnailUrl: '',
-        name: 'Apple MacBook Pro 16 M3 Max',
-        price: '$3,499.00',
-        stock: 3,
-        isChecked: false,
-      },
-      {
-        id: 21,
-        thumbnailUrl: '',
-        name: 'Apple MacBook Pro 14 M3 Pro',
-        price: '$2,499.00',
-        stock: 4,
-        isChecked: false,
-      },
-    ],
+    label: 'Redeploy',
+    iconName: 'i-youcan-arrows-clockwise',
+    tooltip: 'Redeploy',
   },
   {
-    id: 2,
-    thumbnailUrl: '',
-    name: 'Apple iMac',
-    price: '$1,499.00',
-    stock: 2,
-    isChecked: false,
+    label: 'Copy URL',
+    iconName: 'i-youcan-copy',
+    tooltip: 'Copy URL',
+  },
+  {
+    label: 'Source Code',
+    iconName: 'i-youcan-code',
+    tooltip: 'Source Code',
   },
 ];
 
-const showPicker = ref(false);
-const selectedResources = ref<Resource[]>([]);
-const resources = ref<Resource[]>([]);
-const isLoading = ref(true);
-
-const onConfirm = (selectedRes: Resource[]) => {
-  selectedResources.value = selectedRes;
-  showPicker.value = false;
-  isLoading.value = true;
-
-  const emptyResources: Resource[] = [];
-  resources.value = emptyResources;
+const domainStatuses: { [k: string]: StaticStatusDefinition } = {
+  ready: {
+    color: '#52E2C0',
+    label: 'Ready',
+    labelColor: '#2d4236',
+  },
+  error: {
+    color: '#EE0200',
+    label: 'Error',
+    labelColor: '#f8dcdc',
+  },
+  building: {
+    color: '#ffc35c',
+    label: 'Building',
+    labelColor: '#574811',
+  },
+  queued: {
+    color: '#999999',
+    label: 'Queued',
+    labelColor: '#fff2f2',
+  },
 };
 
-const fetchResources = async () => {
-  showPicker.value = true;
-  setTimeout(() => {
-    resources.value = MOCK_RESOURCES;
-    isLoading.value = false;
-  }, 2000);
+const data = ref<TableData[]>([
+  {
+    row: {
+      id: 'eihabkhan.com',
+      domain: 'eihabkhan.com',
+      status: {
+        variant: 'static-status',
+        data: {
+          status: domainStatuses.building,
+        },
+      },
+      branch: 'main',
+    },
+  },
+  {
+    row: {
+      id: 'supershop.youcan.shop',
+      domain: 'supershop.youcan.shop',
+      status: {
+        variant: 'static-status',
+        data: {
+          status: domainStatuses.queued,
+        },
+      },
+      branch: 'main',
+    },
+  },
+  {
+    row: {
+      id: 'vercel.com',
+      domain: 'vercel.com',
+      status: {
+        variant: 'static-status',
+        data: {
+          status: domainStatuses.ready,
+        },
+      },
+      branch: 'main',
+    },
+  },
+  {
+    row: {
+      domain: 'nextjs.org',
+      status: {
+        variant: 'static-status',
+        data: {
+          status: domainStatuses.error,
+        },
+      },
+      branch: 'feat/next-conf',
+    },
+  },
+]);
+
+const selectedRows = ref<TableData[]>([]);
+
+const handleSort = (column: TableColumn) => {
+  data.value = data.value.sort((a: any, b: any) => {
+    let textA = '';
+    let textB = '';
+
+    if (typeof a.row[column.accessor] === 'string') {
+      textA = (a.row[column.accessor] as string)?.toUpperCase();
+      textB = (b.row[column.accessor] as string)?.toUpperCase();
+    }
+
+    else if (a.row?.status?.data.status.label && b.row?.status?.data.status.label) {
+      textA = a.row?.status?.data.status.label;
+      textB = b.row?.status?.data.status.label;
+    }
+
+    if (textA < textB) {
+      return -1;
+    }
+    if (textA > textB) {
+      return 1;
+    }
+
+    return 0;
+  });
 };
+
+const updateSelectedRows = (data: TableData[]) => {
+  selectedRows.value = data;
+};
+
+const TOTAL = 50;
+const PER_PAGE = 5;
+const TOTAL_PAGES = Math.ceil(TOTAL / PER_PAGE);
+
+const currentPage = ref(2);
+
+const count = computed(() => {
+  const page = currentPage.value - 1;
+
+  return `${(PER_PAGE * page) + 1} - ${(PER_PAGE * page) + PER_PAGE}`;
+});
+
+function handlePaginationNavigation(pageNumber: number) {
+  currentPage.value = pageNumber;
+}
 </script>
 
 <template>
-  <div className="container">
-    <ResourcePicker
-      v-model:visible="showPicker"
-      :resources="resources"
-      stock-label="in stock"
-      :is-loading="isLoading"
-      @confirm="onConfirm"
+  <div class="container">
+    <Table
+      :columns="columns"
+      :data="data"
+      :actions="actions"
+      actions-text="My actions"
+      :selectable="true"
+      :selected-rows="selectedRows"
+      @sort="handleSort"
+      @update:selected-rows="updateSelectedRows"
+    >
+      <template #domain="row">
+        <div> {{ row.row.domain.value }} | {{ row.row.branch.value }}</div>
+      </template>
+    </Table>
+    <PaginationBar
+      :current="currentPage"
+      :size="TOTAL_PAGES"
+      :count="count"
+      :total="TOTAL"
+      @update:current="handlePaginationNavigation"
     />
-    <PrimaryButton @click="fetchResources">
-      <span>Open Picker</span>
-    </PrimaryButton>
-    <div v-if="selectedResources.length > 0" class="selection">
-      <pre>{{ selectedResources }}</pre>
-    </div>
   </div>
 </template>
 
 <style scoped>
 .container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  max-width: 300px;
-  gap: 16px;
-}
-
-.selection {
-  padding: 16px;
-  border: 1px solid var(--gray-200);
-  border-radius: 8px;
-  background-color: var(--vp-c-bg);
-}
-
-:is(.dark) .selection {
-  border-color: var(--gray-700);
+  max-width: 60%;
+  margin: 60px auto;
 }
 </style>
