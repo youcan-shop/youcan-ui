6bd58cf70e00c22ec79ecc79e8213bbe44dd4061
GhitaNektt <152861043+GhitaNektt@users.noreply.github.com>
2024-01-02 14:58:11 +0100

    YSHOP2-1221: Added thickness prop to divider (#200)
    Co-authored-by: Ghita Nekt <ghitanekt@admins-MacBook-Pro.local>

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -1,94 +1,10 @@
 <script setup lang="ts">
-import 'uno.css';
-import '../assets/main.css';
-import { ref } from 'vue';
-import type { StaticStatusDefinition } from '~/components/Status/types';
-import { Dropdown, MultiselectDropdown, StaticStatus } from '~/components';
-
-const category = ref(null);
-const desiredLanguages = ref([]);
-
-const nameList = [
-  'Time', 'Past', 'Future', 'Dev',
-  'Fly', 'Flying', 'Soar', 'Soaring', 'Power', 'Falling',
-  'Fall', 'Jump', 'Cliff', 'Mountain', 'Rend', 'Red', 'Blue',
-  'Green', 'Yellow', 'Gold', 'Demon'];
-
-const fruits: StaticStatusDefinition[] = [
-  {
-    color: '#ffdecb',
-    label: '',
-    labelColor: '#35192b',
-  },
-  {
-    color: '#fffad2',
-    label: '',
-    labelColor: '#555022',
-  },
-  {
-    color: '#cbffd3',
-    label: '',
-    labelColor: '#2c4730',
-  },
-  {
-    color: '#EEAAAA',
-    label: '',
-    labelColor: '#7B1919',
-  },
-];
-
-const getStatus = (text: string): StaticStatusDefinition => {
-  const status = fruits[Math.floor(Math.random() * (fruits.length))];
-  status.label = text;
-
-  return status;
-};
-const items = Array.from({ length: 100 }, () => {
-  return {
-    label: nameList[Math.floor(Math.random() * (nameList.length - 1))],
-    value: Math.floor(Math.random() * 10000000),
-  };
-});
-
-const languages = [
-  { label: 'Arabic', value: 1 },
-  { label: 'English', value: 2 },
-  { label: 'German', value: 3 },
-  { label: 'Arabic', value: 4 },
-  { label: 'English', value: 5 },
-  { label: 'German', value: 6 },
-  { label: 'Arabic', value: 7 },
-  { label: 'English', value: 8 },
-  { label: 'German', value: 9 },
-];
+import { Divider } from '~/components';
 </script>
 
 <template>
-  <div class="dropdown-container">
-    <Dropdown
-      v-model="category"
-      searchable
-      :items="items"
-      placeholder="Select category"
-    >
-      <template #accessory="item">
-        <div class="status">
-          <StaticStatus :status="getStatus(item.label)" />
-        </div>
-      </template>
-    </Dropdown>
-    <MultiselectDropdown
-      v-model="desiredLanguages"
-      :items="languages"
-      label="Desired languages"
-      icon="i-youcan-translate"
-    >
-      <template #accessory="item">
-        <div class="status">
-          {{ item.value }}
-        </div>
-      </template>
-    </MultiselectDropdown>
+  <div class="container">
+    <Divider thickness="light" />
   </div>
 </template>
 
