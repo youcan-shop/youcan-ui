d88a74e7b286bef4b3c460e78d7cbd5b4ce09be6
Ghita Nekt <ghitanekt@admins-MacBook-Pro.local>
2024-01-05 15:37:25 +0100

    Added custom types for components props

--- packages/vue3/src/components/Divider/types.ts
+++ packages/vue3/src/components/Divider/types.ts
@@ -1,3 +1,11 @@
 export type Orientation = 'horizontal' | 'vertical';
+
 export type Style = 'solid' | 'dotted' | 'dashed';
+
 export type Thickness = 'light' | 'regular' | 'thick';
+
+export interface DividerProps {
+  orientation?: Orientation
+  borderStyle?: Style
+  thickness?: Thickness
+}
