03af6497741e2e9e3e780542577f73ff151c482f
GhitaNektt <152861043+GhitaNektt@users.noreply.github.com>
2024-01-08 16:11:34 +0100

    Added custom types for table and tag props (#229)
    Co-authored-by: Ghita Nekt <ghitanekt@admins-MacBook-Pro.local>

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -2,43 +2,164 @@
 import 'uno.css';
 import '../assets/main.css';
 import { ref } from 'vue';
-import { Slider, Spinner, StaticStatus } from '~/components';
+import { Table, Tag } from '~/components';
+import type { TableActions, TableColumn, TableData } from '~/components/Table/types';
 import type { StaticStatusDefinition } from '~/components/Status/types';
+import type { TagItemValue } from '~/components/Tag/types';
 
-const value = ref(100);
-const fruits: StaticStatusDefinition[] = [
+const columns: TableColumn[] = [
+  { label: 'Domain', accessor: 'domain', sortable: 'desc' },
+  { label: 'Status', accessor: 'status', sortable: 'desc' },
+  { label: 'Git Branch', accessor: 'branch', sortable: 'desc' },
+];
+
+const actions: TableActions[] = [
   {
-    color: '#ffdecb',
-    label: 'Peach 🍑',
-    labelColor: '#35192b',
+    label: 'Redeploy',
+    iconName: 'i-youcan-arrows-clockwise',
+    tooltip: 'Redeploy',
   },
   {
-    color: '#fffad2',
-    label: 'Banana 🍌',
-    labelColor: '#555022',
+    label: 'Copy URL',
+    iconName: 'i-youcan-copy',
+    tooltip: 'Copy URL',
   },
   {
-    color: '#cbffd3',
-    label: 'Kiwi 🥝',
-    labelColor: '#2c4730',
+    label: 'Source Code',
+    iconName: 'i-youcan-code',
+    tooltip: 'Source Code',
   },
 ];
+
+const domainStatuses: { [k: string]: StaticStatusDefinition } = {
+  ready: {
+    color: '#52E2C0',
+    label: 'Ready',
+    labelColor: '#2d4236',
+  },
+  error: {
+    color: '#EE0200',
+    label: 'Error',
+    labelColor: '#f8dcdc',
+  },
+  building: {
+    color: '#ffc35c',
+    label: 'Building',
+    labelColor: '#574811',
+  },
+  queued: {
+    color: '#999999',
+    label: 'Queued',
+    labelColor: '#fff2f2',
+  },
+};
+
+const data = ref<TableData[]>([
+  {
+    row: {
+      id: 'eihabkhan.com',
+      domain: 'eihabkhan.com',
+      status: {
+        variant: 'static-status',
+        data: {
+          status: domainStatuses.building,
+        },
+      },
+      branch: 'main',
+    },
+  },
+  {
+    row: {
+      id: 'supershop.youcan.shop',
+      domain: 'supershop.youcan.shop',
+      status: {
+        variant: 'static-status',
+        data: {
+          status: domainStatuses.queued,
+        },
+      },
+      branch: 'main',
+    },
+  },
+  {
+    row: {
+      id: 'vercel.com',
+      domain: 'vercel.com',
+      status: {
+        variant: 'static-status',
+        data: {
+          status: domainStatuses.ready,
+        },
+      },
+      branch: 'main',
+    },
+  },
+  {
+    row: {
+      domain: 'nextjs.org',
+      status: {
+        variant: 'static-status',
+        data: {
+          status: domainStatuses.error,
+        },
+      },
+      branch: 'feat/next-conf',
+    },
+  },
+]);
+
+const selectedRows = ref<TableData[]>([]);
+
+const handleSort = (column: TableColumn) => {
+  data.value = data.value.sort((a: any, b: any) => {
+    let textA = '';
+    let textB = '';
+
+    if (typeof a.row[column.accessor] === 'string') {
+      textA = (a.row[column.accessor] as string)?.toUpperCase();
+      textB = (b.row[column.accessor] as string)?.toUpperCase();
+    }
+
+    else if (a.row?.status?.data.status.label && b.row?.status?.data.status.label) {
+      textA = a.row?.status?.data.status.label;
+      textB = b.row?.status?.data.status.label;
+    }
+
+    if (textA < textB) {
+      return -1;
+    }
+    if (textA > textB) {
+      return 1;
+    }
+
+    return 0;
+  });
+};
+
+const updateSelectedRows = (data: TableData[]) => {
+  selectedRows.value = data;
+};
+const preferredLanguages = ref<TagItemValue[]>([
+  { label: 'JavaScript' },
+]);
 </script>
 
 <template>
   <div class="container">
-    <Slider
-      v-model="value"
-      suffix=""
-      :max="500"
-      :min="0"
+    <Table
+      :columns="columns"
+      :data="data"
+      :actions="actions"
+      :selectable="true"
+      :selected-rows="selectedRows"
+      @sort="handleSort"
+      @update:selected-rows="updateSelectedRows"
     />
-    <Spinner
-      color="var(--brand-500)"
-      size="40px"
-      label=""
+    <Tag
+      v-model="preferredLanguages"
+      placeholder="Your favorite programming languages"
+      :max="3"
     />
-    <StaticStatus v-for="fruit in fruits" :key="fruit.color" :status="fruit" />
   </div>
 </template>
 
@@ -46,7 +167,7 @@ const fruits: StaticStatusDefinition[] = [
 .container {
   display: flex;
   flex-direction: column;
-  gap: 30px;
   margin: 10%;
+  gap: 20px;
 }
 </style>
--- packages/vue3/src/components/Tag/types.ts
+++ packages/vue3/src/components/Tag/types.ts
@@ -8,3 +8,17 @@ export interface TagItemValue {
 export interface UniqueTagItem extends TagItemValue {
   id: string
 }
+
+export interface TagProps {
+  modelValue: TagItemValue[]
+  type?: TagItemType
+  max?: number
+  placeholder?: string
+  disabled?: boolean
+  error?: boolean
+}
+
+export interface TagItemProps {
+  modelValue: TagItemValue
+  type?: TagItemType
+}
