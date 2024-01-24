5fe1881c72b00ca776d372d46f2a446673fcafd5
mosarabi <149577299+mosarabi@users.noreply.github.com>
2024-01-24 10:45:18 +0100

    ToastContainer > Add toast.show trigger (#244)

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -1,24 +1,44 @@
 <script setup lang="ts">
 import 'uno.css';
 import '../assets/main.css';
-import { ref } from 'vue';
-import { Input, TextArea } from '~/components';
+import { PrimaryButton, ToastContainer, toast } from '~/components';
+import type { ToastOptions } from '~/components/ToastContainer/types';
 
-const comment = ref('');
+const toasts: ToastOptions[] = [
+  {
+    title: 'Info',
+    description: 'Click here to learn more about the exciting enhancements we\'ve made.',
+    type: 'info',
+  },
+  {
+    title: 'Success',
+    description: 'Your profile information has been successfully updated.',
+    type: 'success',
+  },
+  {
+    title: 'Warning',
+    description: 'Please check your internet connection.',
+    type: 'warning',
+  },
+  {
+    title: 'Error',
+    description: 'Unable to save data, check again later.',
+    type: 'error',
+  },
+];
+
+const handleClick = () => {
+  const toastOptions: ToastOptions = toasts[Math.floor(Math.random() * toasts.length)];
+  toast.show(toastOptions);
+};
 </script>
 
 <template>
-  <div class="container">
-    <TextArea
-      v-model="comment"
-      placeholder="Leave your comment"
-    />
-    <Input
-      v-model="comment"
-      placeholder="Leave your comment"
-      type="password"
-      can-show
-    />
+  <ToastContainer :limit="3" />
+  <div className="container">
+    <PrimaryButton @click="handleClick">
+      Show Toast
+    </PrimaryButton>
   </div>
 </template>
 
--- packages/vue3/src/components/ToastContainer/index.ts
+++
@@ -1,6 +0,0 @@
-import type { ToastOptions } from './types';
-
-export const showToast = (toastOptions: ToastOptions) => {
-  const data = { id: 'show-toast', options: toastOptions };
-  window.postMessage(data);
-};
