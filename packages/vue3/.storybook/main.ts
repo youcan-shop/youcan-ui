ae8909c066055fbd020cfb437b9b303374b2a9d3
Eihab Khan <143792300+eihabkhan1@users.noreply.github.com>
2024-01-25 14:59:28 +0100

    fix unocss error

--- packages/vue3/.storybook/main.ts
+++ packages/vue3/.storybook/main.ts
@@ -1,18 +1,26 @@
-import type { StorybookConfig } from "@storybook/vue3-vite";
+import path from 'path';
+import Unocss from 'unocss/vite';
 
-const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
+module.exports = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    "@storybook/addon-interactions"
   ],
-  framework: {
-    name: "@storybook/vue3-vite",
-    options: {},
+  "framework": {
+    "name": "@storybook/vue3-vite",
+    "options": {}
   },
-  docs: {
-    autodocs: "tag",
+  "docs": {
+    "docsPage": false
   },
-};
-export default config;
+  async viteFinal(config, { configType }) {
+    config.resolve.alias['~'] =  `${path.resolve(__dirname, 'src')}/`;
+    config.plugins.push(Unocss);
+    return config;
+  }
+}
