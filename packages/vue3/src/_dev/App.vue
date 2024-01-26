30a5e33d2bd8cfabbbf0abf6b27a65a92e1a0f10
mosarabi <149577299+mosarabi@users.noreply.github.com>
2024-01-26 10:30:04 +0100

    Add Drawer component (#248)

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -1,63 +1,48 @@
 <script setup lang="ts">
 import 'uno.css';
 import '../assets/main.css';
-import { ref, watch } from 'vue';
-import { MediaInput, UploadedMedia } from '~/components';
+import { ref } from 'vue';
+import { Drawer, PrimaryButton } from '~/components';
 
-const attachments = ref<File[]>([]);
-const disabled = ref(false);
-const limit = 4;
-
-const checkLimit = () => {
-  disabled.value = attachments.value.length >= limit;
-};
-
-const deleteFile = (file: File) => {
-  const idx = attachments.value.indexOf(file);
-  if (idx > -1) {
-    attachments.value.splice(idx, 1);
-    checkLimit();
-  }
-};
-watch(attachments, () => {
-  checkLimit();
-});
+const show = ref(false);
 </script>
 
 <template>
+  <Drawer v-model:visible="show" position="left">
+    <p v-for="index in 50" :key="index" class="content">
+      The quick brown fox jumps over the lazy dog.
+    </p>
+    <template #footer>
+      <div class="actions">
+        <PrimaryButton @click="show = false;">
+          <span>Save</span>
+        </PrimaryButton>
+      </div>
+    </template>
+  </Drawer>
   <div class="container">
-    <div class="files-grid">
-      <UploadedMedia
-        v-for="(attachment, index) in attachments"
-        :key="index"
-        :file="attachment"
-        @delete="deleteFile(attachment)"
-      />
-    </div>
-    <MediaInput v-model="attachments" :limit="limit" :disabled="disabled" />
-    <div class="files-grid">
-      <UploadedMedia
-        file="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg"
-      />
-    </div>
+    <PrimaryButton @click="show = true;">
+      <span>Open Drawer</span>
+    </PrimaryButton>
   </div>
 </template>
 
 <style scoped>
 .container {
   display: flex;
-  position: relative;
-  flex-direction: column;
   align-items: center;
   justify-content: center;
-  width: 300px;
-  min-height: 140px;
+  width: 100vw;
+  height: 100vh;
 }
 
-.file-grid {
-  position: absolute;
-  right: 0;
-  bottom: 0;
-  left: 0;
+.content {
+  margin: 0;
+  text-align: center;
+}
+
+.actions {
+  display: flex;
+  flex-direction: row-reverse;
 }
 </style>
