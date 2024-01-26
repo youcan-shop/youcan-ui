b53317cb10b728bc004abffd60d005bf96741397
mosarabi <149577299+mosarabi@users.noreply.github.com>
2024-01-26 10:03:20 +0100

    Drawer rtl adaptation

--- packages/vue3/src/components/Drawer/Drawer.vue
+++ packages/vue3/src/components/Drawer/Drawer.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import { onMounted, onUnmounted, ref, useSlots } from 'vue';
+import { onMounted, onUnmounted, useSlots } from 'vue';
 import type { DrawerProps } from './types';
 import Overlay from '~/components/Overlay/Overlay.vue';
 
@@ -9,8 +9,6 @@ const props = withDefaults(defineProps<DrawerProps>(), {
 });
 
 const emit = defineEmits(['update:visible']);
-
-const slideDirection = ref('100%');
 
 const slots = useSlots();
 
@@ -25,9 +23,6 @@ const handleKeypress = (event: KeyboardEvent) => {
 };
 
 onMounted(() => {
-  if (props.position === 'left') {
-    slideDirection.value = '-100%';
-  }
   window.addEventListener('keydown', handleKeypress);
 });
 
@@ -39,7 +34,7 @@ onUnmounted(() => {
 <template>
   <Transition name="fade">
     <Overlay v-show="visible" class="drawer-overlay" :class="position" @on-backdrop-click="close">
-      <Transition name="slide">
+      <Transition :name="position">
         <div v-if="visible" class="drawer">
           <div class="header">
             <div class="close" @click="close">
@@ -65,8 +60,80 @@ $duration: 0.45s;
   flex-direction: row-reverse;
   align-items: flex-start;
 
+  .drawer {
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    width: max-content;
+    max-width: 80vw;
+    overflow: hidden;
+    border: 1px solid var(--gray-200);
+    background-color: var(--base-white);
+    box-shadow: var(--shadow-md-gray);
+
+    @media only screen and (max-width: 992px) {
+      max-width: 100vw;
+    }
+
+    > * {
+      box-sizing: border-box;
+      width: 100%;
+    }
+
+    .header {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      padding: 10px 20px;
+      border-bottom: 1px solid var(--gray-100);
+
+      .close {
+        cursor: pointer;
+
+        i {
+          color: var(--gray-500);
+        }
+      }
+    }
+
+    .footer {
+      padding: 10px 20px;
+      border-top: 1px solid var(--gray-100);
+    }
+
+    .body {
+      flex: 1;
+      padding: 14px 20px;
+      overflow-y: auto;
+      font: var(--text-md-regular);
+
+      &::-webkit-scrollbar {
+        width: 4px;
+      }
+
+      &::-webkit-scrollbar-track {
+        background: transparent;
+      }
+
+      &::-webkit-scrollbar-thumb {
+        border-radius: 2px;
+        background-color: var(--brand-500);
+      }
+
+      &::-webkit-scrollbar-thumb:hover {
+        background-color: var(--brand-500);
+      }
+    }
+  }
+
   &.left {
     flex-direction: row;
+
+    .drawer {
+      .header {
+        flex-direction: row-reverse;
+      }
+    }
   }
 
   > :deep(.body) {
@@ -74,77 +141,6 @@ $duration: 0.45s;
     flex-direction: column;
     height: 100%;
     margin: 0;
-  }
-}
-
-.drawer {
-  display: flex;
-  flex: 1;
-  flex-direction: column;
-  width: max-content;
-  max-width: 80vw;
-  overflow: hidden;
-  border: 1px solid var(--gray-200);
-  background-color: var(--base-white);
-  box-shadow: var(--shadow-md-gray);
-
-  @media only screen and (max-width: 992px) {
-    max-width: 100vw;
-  }
-
-  > * {
-    box-sizing: border-box;
-    width: 100%;
-  }
-
-  .header {
-    display: flex;
-    flex-direction: row-reverse;
-    align-items: center;
-    padding: 10px 20px;
-    border-bottom: 1px solid var(--gray-100);
-
-    .title {
-      color: var(--gray-700);
-      font: var(--text-md-medium);
-    }
-
-    .close {
-      cursor: pointer;
-
-      i {
-        color: var(--gray-500);
-      }
-    }
-  }
-
-  .footer {
-    padding: 10px 20px;
-    border-top: 1px solid var(--gray-100);
-  }
-
-  .body {
-    flex: 1;
-    padding: 14px 20px;
-    overflow-y: auto;
-    font: var(--text-md-regular);
-
-    &::-webkit-scrollbar {
-      width: 4px;
-    }
-
-    &::-webkit-scrollbar-track {
-      background: transparent;
-    }
-
-    &::-webkit-scrollbar-thumb {
-      border-radius: 2px;
-      background-color: var(--brand-500);
-    }
-
-    &::-webkit-scrollbar-thumb:hover {
-      background-color: var(--brand-500);
-    }
   }
 }
 
@@ -166,21 +162,51 @@ $duration: 0.45s;
   }
 }
 
-.slide-enter-active {
-  animation: slide $duration ease-in-out;
+.right-enter-active {
+  animation: right $duration ease-in-out;
 }
 
-.slide-leave-active {
-  animation: slide $duration reverse ease-in-out;
+.right-leave-active {
+  animation: right $duration reverse ease-in-out;
 }
 
-@keyframes slide {
+@keyframes right {
   0% {
-    transform: translateX(v-bind(slideDirection));
+    transform: translateX(100%);
   }
 
   100% {
     transform: translateX(0%);
+  }
+}
+
+.left-enter-active {
+  animation: left $duration ease-in-out;
+}
+
+.left-leave-active {
+  animation: left $duration reverse ease-in-out;
+}
+
+@keyframes left {
+  0% {
+    transform: translateX(-100%);
+  }
+
+  100% {
+    transform: translateX(0%);
+  }
+}
+
+html[dir="rtl"] {
+  .drawer-overlay {
+    flex-direction: row;
+    align-items: flex-start;
+    direction: rtl;
+
+    &.left {
+      flex-direction: row-reverse;
+    }
   }
 }
 </style>
