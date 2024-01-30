a20fce5675ccb5ee68aaffa6d9dfa47b16315511
mosarabi <149577299+mosarabi@users.noreply.github.com>
2024-01-30 15:38:55 +0100

    Draggable > stories

--- packages/vue3/src/components/Divider/Divider.stories.ts
+++ packages/vue3/src/components/Divider/Divider.stories.ts
@@ -1,16 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/vue3';
-import Divider from './Divider.vue';
+import { Divider } from '~/components';
 
 type Story = StoryObj<typeof Divider>;
 const meta: Meta<typeof Divider> = {
   title: 'Application/Divider',
   component: Divider,
   tags: ['divider', 'hr', 'vr', 'separator'],
-  args: {
-    orientation: 'horizontal',
-    borderStyle: 'solid',
-    thickness: 'regular',
-  },
   argTypes: {
     orientation: {
       control: {
@@ -31,6 +26,11 @@ const meta: Meta<typeof Divider> = {
       options: ['light', 'regular', 'thick'],
     },
   },
+  args: {
+    orientation: 'horizontal',
+    borderStyle: 'solid',
+    thickness: 'regular',
+  },
 };
 
 export const Default: Story = {
@@ -39,7 +39,7 @@ export const Default: Story = {
     setup() {
       return { args };
     },
-    template: '<Divider v-bind="args"/>',
+    template: '<Divider v-bind="args" />',
   }),
 };
 
