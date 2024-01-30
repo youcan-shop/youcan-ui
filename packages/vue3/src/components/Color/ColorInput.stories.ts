217943389cd5ad8bf09d9dc18107fbcdb0e05ff5
mosarabi <149577299+mosarabi@users.noreply.github.com>
2024-01-30 12:17:28 +0100

    ColorInput > stories

--- packages/vue3/src/components/Color/ColorInput.stories.ts
+++ packages/vue3/src/components/Color/ColorInput.stories.ts
@@ -1,26 +1,26 @@
-import type { Meta, StoryObj } from '@storybook/vue3';
+import type { Meta } from '@storybook/vue3';
 import ColorInput from './ColorInput.vue';
 
-type Story = StoryObj<typeof ColorInput>;
 const meta: Meta<typeof ColorInput> = {
   title: 'Application/ColorInput',
   component: ColorInput,
-  tags: ['ColorInput', 'ColorPicker'],
-};
-
-export const Default: Story = {
-  render: args => ({
-    components: { ColorInput },
-    setup() {
-      return { args };
+  tags: ['color', 'color input', 'color picker'],
+  argTypes: {
+    preserveTransparency: {
+      control: 'boolean',
     },
-    template: `
-    <ColorInput
-      v-model="color"
-      :preserve-transparency="false"
-    />
-  `,
-  }),
+  },
 };
 
 export default meta;
+
+const Template = (args: Record<string, unknown>, { argTypes }: Record<string, Record<string, unknown>>) => ({
+  props: Object.keys(argTypes),
+  data() {
+    return { color: '#505060' };
+  },
+  components: { ColorInput },
+  template: ' <ColorInput v-model="color" /> ',
+});
+
+export const Default = Template.bind({});
