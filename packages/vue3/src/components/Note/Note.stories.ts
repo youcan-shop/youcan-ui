31f070bfd5a22324833f248b836692688b2e9022
mosarabi <149577299+mosarabi@users.noreply.github.com>
2024-02-01 16:18:23 +0100

    MultiSwitch > stories

--- packages/vue3/src/components/Note/Note.stories.ts
+++ packages/vue3/src/components/Note/Note.stories.ts
@@ -5,6 +5,18 @@ const meta: Meta<typeof Note> = {
   title: 'Application/Note',
   component: Note,
   tags: ['application', 'display', 'note'],
+  argTypes: {
+    icon: {
+      table: {
+        disable: true,
+      },
+    },
+    content: {
+      table: {
+        disable: true,
+      },
+    },
+  },
 };
 
 export default meta;
