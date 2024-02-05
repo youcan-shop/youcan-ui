12a676c05559c06b2980942e5eda0ef6c59ca8b3
mosarabi <149577299+mosarabi@users.noreply.github.com>
2024-02-05 17:13:43 +0100

    StaticStatus > stories

---
+++ packages/vue3/src/components/Status/StaticStatus.stories.ts
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/vue3';
+import { StaticStatus } from '~/components';
+
+type Story = StoryObj<typeof StaticStatus>;
+const meta: Meta<typeof StaticStatus> = {
+  title: 'Application/Status/Static',
+  component: StaticStatus,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['static', 'static status', 'status'],
+  args: {
+    status: {
+      color: '#ffdecb',
+      label: 'Peach 🍑',
+      labelColor: '#35192b',
+    },
+  },
+};
+
+export const Default: Story = {
+  render: args => ({
+    components: { StaticStatus },
+    setup() {
+      return { args };
+    },
+    template: '<StaticStatus v-bind="args" />',
+  }),
+};
+
+export default meta;
