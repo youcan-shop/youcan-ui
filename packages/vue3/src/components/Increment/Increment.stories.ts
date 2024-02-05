a3e37144d9705f83e5c4f789ed53336ed95ca033
mosarabi <149577299+mosarabi@users.noreply.github.com>
2024-02-05 10:06:01 +0100

    Increment > stories

--- packages/vue3/src/components/Increment/Increment.stories.ts
+++ packages/vue3/src/components/Increment/Increment.stories.ts
@@ -1,29 +1,42 @@
-/* eslint-disable no-console */
-import type { Meta } from '@storybook/vue3';
-import Increment_ from './Increment.vue';
+import type { Meta, StoryObj } from '@storybook/vue3';
+import { ref } from 'vue';
+import Increment from './Increment.vue';
 
-const meta: Meta<typeof Increment_> = {
+type Story = StoryObj<typeof Increment>;
+const meta: Meta<typeof Increment> = {
   title: 'Application/Increment',
-  component: Increment_,
+  component: Increment,
   tags: ['application', 'input', 'number', 'increment'],
+  argTypes: {
+    modelValue: {
+      table: {
+        disable: true,
+      },
+    },
+    id: {
+      table: {
+        disable: true,
+      },
+    },
+  },
+  args: {
+    step: 10,
+    max: 100,
+    min: -10,
+    disabled: false,
+  },
+};
+
+export const Default: Story = {
+  render: args => ({
+    components: { Increment },
+    setup() {
+      const value = ref(10);
+
+      return { args, value };
+    },
+    template: '<Increment v-bind="args" v-model="value" /><span class="sb-c-preview-text">Note: Story not working properly. Value : {{ value }}</span>',
+  }),
 };
 
 export default meta;
-
-const Template = (args: Record<string, unknown>, { argTypes }: Record<string, Record<string, unknown>>) => ({
-  props: Object.keys(argTypes).filter(x => x !== 'value'),
-  data() {
-    return {
-      value: '1',
-      step: 10,
-      max: 100,
-      min: -10,
-      disabled: false,
-    };
-  },
-  components: { Increment_ },
-  template: '<Increment_ v-bind="$props" v-model="value" @update:model-value="action" /><span class="sb-c-preview-text">Note: Story not working properly. Value : {{ value }}</span>',
-  methods: { action: (value: boolean) => console.log(`Increment value: ${value}`) },
-});
-
-export const Default = Template.bind({});
