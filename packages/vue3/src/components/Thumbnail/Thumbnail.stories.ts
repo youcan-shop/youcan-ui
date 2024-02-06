423f10802010a4eb7675305b65c0784ae620688c
mosarabi <149577299+mosarabi@users.noreply.github.com>
2024-02-06 10:33:00 +0100

    Thumbnail > stories

--- packages/vue3/src/components/Thumbnail/Thumbnail.stories.ts
+++ packages/vue3/src/components/Thumbnail/Thumbnail.stories.ts
@@ -1,24 +1,36 @@
-import type { Meta } from '@storybook/vue3';
-import Thumbnail_ from './Thumbnail.vue';
+import type { Meta, StoryObj } from '@storybook/vue3';
+import { Thumbnail } from '~/components';
 
-const meta: Meta<typeof Thumbnail_> = {
+type Story = StoryObj<typeof Thumbnail>;
+const meta: Meta<typeof Thumbnail> = {
   title: 'Application/Thumbnail',
-  component: Thumbnail_,
+  component: Thumbnail,
   tags: ['application', 'display', 'thumbnail', 'image', 'avatar'],
   argTypes: {
     size: {
+      control: { type: 'select' },
       options: ['small', 'large'],
-      control: {
-        type: 'select',
-      },
+    },
+    objectFit: {
+      control: { type: 'select' },
+      options: ['fill', 'contain', 'cover', 'none'],
     },
   },
+  args: {
+    size: 'small',
+    src: 'https://i.imgur.com/YcP0tik.jpeg',
+    objectFit: 'cover',
+    alt: 'YcP0tik',
+  },
+};
+export const Default: Story = {
+  render: args => ({
+    components: { Thumbnail },
+    setup() {
+      return { args };
+    },
+    template: '<Thumbnail  v-bind="args" />',
+  }),
 };
 
 export default meta;
-
-export const Thumbnail = {
-  args: {
-    size: 'small',
-  },
-};
