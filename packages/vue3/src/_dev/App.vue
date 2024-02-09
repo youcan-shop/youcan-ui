e31df4c17b27104a95b480e08fcec7a4b3847a7c
mosarabi <149577299+mosarabi@users.noreply.github.com>
2024-02-09 16:40:12 +0100

    Upload > Add support for videos

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -2,19 +2,47 @@
 import 'uno.css';
 import '../assets/main.css';
 import { ref } from 'vue';
-import { Tag } from '~/components';
-import type { TagItemValue } from '~/types';
+import { MediaInput, UploadedMedia } from '~/components';
 
-const preferredLanguages = ref<TagItemValue[]>([
-  { label: 'JavaScript' },
-]);
+const attachments = ref<File[]>([]);
+const limit = 7;
+
+const deleteFile = (index: number) => {
+  const override = attachments.value.filter((el, i) => i !== index);
+  attachments.value = override;
+};
 </script>
 
 <template>
-  <Tag
-    v-model="preferredLanguages"
-    placeholder="Your favorite programming languages"
-    :max="3"
-    disabled
-  />
+  <div class="container">
+    <div class="files-grid">
+      <UploadedMedia
+        v-for="(attachment, index) in attachments"
+        :key="index"
+        :file="attachment"
+        @delete="deleteFile(index)"
+      />
+    </div>
+    <MediaInput v-model="attachments" :limit="limit" />
+  </div>
 </template>
+
+<style scoped>
+.container {
+  display: flex;
+  position: relative;
+  box-sizing: border-box;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  padding: 30px;
+}
+
+.files-grid {
+  display: grid;
+  grid-template-columns: repeat(4, auto);
+  gap: 10px;
+  margin-bottom: 60px;
+}
+</style>
