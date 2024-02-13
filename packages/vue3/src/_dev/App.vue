5090f9bae0bb3cda4de0b0b0c16c3e4039722864
mosarabi <149577299+mosarabi@users.noreply.github.com>
2024-02-13 10:46:46 +0100

    Dropdown Tag component

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -3,18 +3,47 @@ import 'uno.css';
 import '../assets/main.css';
 import { ref } from 'vue';
 import { Tag } from '~/components';
-import type { TagItemValue } from '~/types';
+import type { TagItemValue } from '~/components/Tag/types';
 
 const preferredLanguages = ref<TagItemValue[]>([
-  { label: 'JavaScript' },
+  { label: 'English', value: 2 },
 ]);
+
+const languages = [
+  { label: 'Arabic', value: 1 },
+  { label: 'English', value: 2 },
+  { label: 'German', value: 3 },
+  { label: 'French', value: 4 },
+  { label: 'Dutch', value: 5 },
+  { label: 'Hindi', value: 6 },
+];
 </script>
 
 <template>
-  <Tag
-    v-model="preferredLanguages"
-    placeholder="Your favorite programming languages"
-    :max="3"
-    disabled
-  />
+  <div class="container">
+    <div>
+      <Tag
+        v-model="preferredLanguages"
+        placeholder="Select programming languages"
+        :max="3"
+        type="dropdown"
+        :items="languages"
+      />
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+}
+
+.container > div {
+  width: 500px;
+}
+</style>
