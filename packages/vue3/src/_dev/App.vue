7c416d2600f5bf717fd0c8bb99ea4c625919e2e5
mosarabi <149577299+mosarabi@users.noreply.github.com>
2024-02-23 10:13:13 +0100

    UI 239: Slider > add range slider min max values (#284)

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -2,21 +2,52 @@
 import 'uno.css';
 import '../assets/main.css';
 import { ref } from 'vue';
-import { Dropdown } from '~/components';
-const category = ref(null);
+import { Slider } from '~/components';
+import type { RangeValue } from '~/types';
 
-const items = [
-  { label: 'Shoes', value: 1 },
-  { label: 'Cosmetics', value: 2 },
-  { label: 'Gym', value: 3 },
-];
+const value = ref(2000);
+const rangeValue = ref<RangeValue>({ min: 200, max: 500 });
 </script>
 
 <template>
-  <Dropdown
-    v-model="category"
-    searchable
-    :items="items"
-    placeholder="Select category"
-  />
+  <div class="container">
+    <div>
+      <Slider
+        v-model="rangeValue"
+        suffix=""
+        :max="2000"
+        :min="200"
+        type="range"
+      />
+      <div>min : {{ rangeValue.min }}  ==> max: {{ rangeValue.max }}</div>
+      <Slider
+        v-model="value"
+        suffix=""
+        :max="2000"
+        :min="200"
+      />
+      <div>{{ value }}</div>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+}
+
+.container > div {
+  display: flex;
+  flex-direction: column;
+  width: 450px;
+  gap: 30px;
+}
+
+.container > div > div {
+  font: var(--text-sm-medium);
+}
+</style>
--- packages/vue3/src/components/Slider/types.ts
+++ packages/vue3/src/components/Slider/types.ts
@@ -1,8 +1,24 @@
-export interface SliderProps {
-  modelValue?: number
-  min?: number
-  max?: number
+type SliderType = 'number' | 'range';
+export interface RangeValue {
+  min: number
+  max: number
+}
+
+interface Props {
+  modelValue?: number | RangeValue
+  min: number
+  max: number
   disabled?: boolean
+  type?: SliderType
+}
+
+export interface RailProps extends Props {
+  modelValue?: number | RangeValue
+  minLabel?: string
+  maxLabel?: string
+}
+
+export interface SliderProps extends Props {
   prefix?: string
   suffix?: string
 }
