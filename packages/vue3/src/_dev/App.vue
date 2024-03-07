ce058e78d51a47a43ab390ab251da6155229a6e5
Ghita Nekt <ghitanekt@admins-MacBook-Pro.local>
2024-03-07 11:24:59 +0100

    small improvments

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -1,33 +1,12 @@
 <script setup lang="ts">
 import 'uno.css';
 import '../assets/main.css';
-import { ref } from 'vue';
-import { Slider } from '~/components';
-import type { RangeValue } from '~/types';
-
-const value = ref(2000);
-const rangeValue = ref<RangeValue>({ min: 200, max: 500 });
+import { Divider } from '~/components';
 </script>
 
 <template>
   <div class="container">
-    <div>
-      <Slider
-        v-model="rangeValue"
-        suffix=""
-        :max="2000"
-        :min="200"
-        type="range"
-      />
-      <div>min : {{ rangeValue.min }}  ==> max: {{ rangeValue.max }}</div>
-      <Slider
-        v-model="value"
-        suffix=""
-        :max="2000"
-        :min="200"
-      />
-      <div>{{ value }}</div>
-    </div>
+    <Divider />
   </div>
 </template>
 
@@ -38,16 +17,5 @@ const rangeValue = ref<RangeValue>({ min: 200, max: 500 });
   justify-content: center;
   width: 100vw;
   height: 100vh;
-}
-
-.container > div {
-  display: flex;
-  flex-direction: column;
-  width: 450px;
-  gap: 30px;
-}
-
-.container > div > div {
-  font: var(--text-sm-medium);
 }
 </style>
