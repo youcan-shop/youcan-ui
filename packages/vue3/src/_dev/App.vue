8c93611e1966a9074d477714547b44e63df93999
mosarabi <149577299+mosarabi@users.noreply.github.com>
2024-03-07 11:08:08 +0100

    UI-280: ToastContainer > Improvement (#290)

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -1,53 +1,37 @@
 <script setup lang="ts">
 import 'uno.css';
 import '../assets/main.css';
-import { ref } from 'vue';
-import { Slider } from '~/components';
-import type { RangeValue } from '~/types';
+import { PrimaryButton, ToastContainer } from '~/components';
+import { toast } from '~/helpers';
+import type { ToastOptions } from '~/types';
 
-const value = ref(2000);
-const rangeValue = ref<RangeValue>({ min: 200, max: 500 });
+const toastOptions: ToastOptions = {
+  title: 'Info',
+  description: 'Click here to learn more about the exciting enhancements we\'ve made.',
+  type: 'info',
+};
+
+const handleClick = () => {
+  toast.show(toastOptions);
+};
 </script>
 
 <template>
-  <div class="container">
-    <div>
-      <Slider
-        v-model="rangeValue"
-        suffix=""
-        :max="2000"
-        :min="200"
-        type="range"
-      />
-      <div>min : {{ rangeValue.min }}  ==> max: {{ rangeValue.max }}</div>
-      <Slider
-        v-model="value"
-        suffix=""
-        :max="2000"
-        :min="200"
-      />
-      <div>{{ value }}</div>
-    </div>
+  <ToastContainer :limit="3" position="bottom-left" />
+  <div className="container">
+    <PrimaryButton @click="handleClick">
+      Show Toast
+    </PrimaryButton>
   </div>
 </template>
 
 <style scoped>
 .container {
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
   width: 100vw;
   height: 100vh;
-}
-
-.container > div {
-  display: flex;
-  flex-direction: column;
-  width: 450px;
-  gap: 30px;
-}
-
-.container > div > div {
-  font: var(--text-sm-medium);
 }
 </style>
