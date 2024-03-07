ad61d13c0a2e6e2316b0b9c480147e96bfd96013
mosarabi <149577299+mosarabi@users.noreply.github.com>
2024-03-07 16:28:19 +0100

    Convert scss to css

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -1,21 +1,99 @@
 <script setup lang="ts">
 import 'uno.css';
 import '../assets/main.css';
-import { Skeleton } from '~/components';
+import { ref } from 'vue';
+import { Drawer, PrimaryButton } from '~/components';
+const showDrawer = ref(false);
 </script>
 
 <template>
+  <Drawer v-model:visible="showDrawer">
+    <div class="drawer-content">
+      <img class="banner" src="https://developer.youcan.shop/banner.webp" alt="">
+      <div class="content">
+        <h1 class="title">
+          Get started with YouCan UI
+        </h1>
+        <p class="description">
+          Create your app using our UI library to insure seemless integration and user experience
+        </p>
+        <ul class="features">
+          <li>👔 Tailored for you</li>
+          <li>🏢 Internal and External Use</li>
+          <li>⚙️ Vue.js and CSS</li>
+        </ul>
+      </div>
+      <PrimaryButton class="cta" @click="showDrawer = false">
+        <span>Get Started</span>
+      </PrimaryButton>
+    </div>
+  </Drawer>
   <div class="container">
-    <Skeleton type="card" />
+    <PrimaryButton @click="showDrawer = true">
+      <span>Open Drawer</span>
+    </PrimaryButton>
   </div>
 </template>
 
 <style scoped>
 .container {
+  display: flex;
   align-items: center;
   justify-content: center;
-  width: 40vw;
-  height: 40vh;
-  margin: 10%;
+  min-width: 50%;
+  padding: 16px;
+}
+
+.drawer-content {
+  display: flex;
+  flex-direction: column;
+  width: 50vw;
+  height: 100%;
+}
+
+.drawer-content .banner {
+  width: 100%;
+  height: 60%;
+  border: 1px solid var(--gray-200);
+  border-radius: 12px;
+  object-fit: cover;
+}
+
+.drawer-content .content {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.content .title {
+  text-align: center;
+}
+
+.content .description {
+  margin: 0;
+  color: var(--gray-500);
+  text-align: center;
+}
+
+.content .features {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  margin-top: 50px;
+  list-style: none;
+  gap: 16px;
+}
+
+.content .features li {
+  margin: 0;
+  padding: 16px;
+  border: 1px solid var(--gray-200);
+  border-radius: 8px;
+}
+
+.drawer-content .cta {
+  width: 100%;
+  min-height: 55px;
 }
 </style>
--- packages/vue3/src/components/Drawer/Drawer.vue
+++ packages/vue3/src/components/Drawer/Drawer.vue
@@ -54,102 +54,102 @@ onUnmounted(() => {
 </template>
 
 <style scoped lang="scss">
-$duration: 0.45s;
-
 .drawer-overlay {
+  --duration: 0.45s;
+
   flex-direction: row-reverse;
   align-items: flex-start;
+}
 
-  .drawer {
-    display: flex;
-    flex: 1;
-    flex-direction: column;
-    width: max-content;
-    max-width: 80vw;
-    overflow: hidden;
-    border: 1px solid var(--gray-200);
-    background-color: var(--base-white);
-    box-shadow: var(--shadow-md-gray);
+.drawer-overlay .drawer {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  width: max-content;
+  max-width: 80vw;
+  overflow: hidden;
+  border: 1px solid var(--gray-200);
+  background-color: var(--base-white);
+  box-shadow: var(--shadow-md-gray);
+}
 
-    @media only screen and (max-width: 992px) {
-      max-width: 100vw;
-    }
-
-    > * {
-      box-sizing: border-box;
-      width: 100%;
-    }
-
-    .header {
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      padding: 10px 20px;
-      border-bottom: 1px solid var(--gray-100);
-
-      .close {
-        cursor: pointer;
-
-        i {
-          color: var(--gray-500);
-        }
-      }
-    }
-
-    .footer {
-      padding: 10px 20px;
-      border-top: 1px solid var(--gray-100);
-    }
-
-    .body {
-      flex: 1;
-      padding: 14px 20px;
-      overflow-y: auto;
-      font: var(--text-md-regular);
-
-      &::-webkit-scrollbar {
-        width: 4px;
-      }
-
-      &::-webkit-scrollbar-track {
-        background: transparent;
-      }
-
-      &::-webkit-scrollbar-thumb {
-        border-radius: 2px;
-        background-color: var(--brand-500);
-      }
-
-      &::-webkit-scrollbar-thumb:hover {
-        background-color: var(--brand-500);
-      }
-    }
+@media only screen and (max-width: 992px) {
+  .drawer-overlay .drawer {
+    max-width: 100vw;
   }
+}
 
-  &.left {
-    flex-direction: row;
+.drawer-overlay .drawer > * {
+  box-sizing: border-box;
+  width: 100%;
+}
 
-    .drawer {
-      .header {
-        flex-direction: row-reverse;
-      }
-    }
-  }
+.drawer-overlay .drawer .header {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 10px 20px;
+  border-bottom: 1px solid var(--gray-100);
+}
 
-  > :deep(.body) {
-    display: flex;
-    flex-direction: column;
-    height: 100%;
-    margin: 0;
-  }
+.drawer-overlay .drawer .header .close {
+  cursor: pointer;
+}
+
+.drawer-overlay .drawer .header .close i {
+  color: var(--gray-500);
+}
+
+.drawer-overlay .drawer .footer {
+  padding: 10px 20px;
+  border-top: 1px solid var(--gray-100);
+}
+
+.drawer-overlay .drawer .body {
+  flex: 1;
+  padding: 14px 20px;
+  overflow-y: auto;
+  font: var(--text-md-regular);
+}
+
+.drawer-overlay .drawer .body::-webkit-scrollbar {
+  width: 4px;
+}
+
+.drawer-overlay .drawer .body::-webkit-scrollbar-track {
+  background: transparent;
+}
+
+.drawer-overlay .drawer .body::-webkit-scrollbar-thumb {
+  border-radius: 2px;
+  background-color: var(--brand-500);
+}
+
+.drawer-overlay .drawer .body::-webkit-scrollbar-thumb:hover {
+  background-color: var(--brand-500);
+}
+
+.drawer-overlay.left {
+  flex-direction: row;
+}
+
+.drawer-overlay.left .drawer .header {
+  flex-direction: row-reverse;
+}
+
+.drawer-overlay > :deep(.body) {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  margin: 0;
 }
 
 .fade-enter-active {
-  animation: fade $duration ease-in-out;
+  animation: fade var(--duration) ease-in-out;
 }
 
 .fade-leave-active {
-  animation: fade $duration reverse ease-in-out;
+  animation: fade var(--duration) reverse ease-in-out;
 }
 
 @keyframes fade {
@@ -163,11 +163,11 @@ $duration: 0.45s;
 }
 
 .right-enter-active {
-  animation: right $duration ease-in-out;
+  animation: right var(--duration) ease-in-out;
 }
 
 .right-leave-active {
-  animation: right $duration reverse ease-in-out;
+  animation: right var(--duration) reverse ease-in-out;
 }
 
 @keyframes right {
@@ -181,11 +181,11 @@ $duration: 0.45s;
 }
 
 .left-enter-active {
-  animation: left $duration ease-in-out;
+  animation: left var(--duration) ease-in-out;
 }
 
 .left-leave-active {
-  animation: left $duration reverse ease-in-out;
+  animation: left var(--duration) reverse ease-in-out;
 }
 
 @keyframes left {
@@ -198,15 +198,13 @@ $duration: 0.45s;
   }
 }
 
-html[dir="rtl"] {
-  .drawer-overlay {
-    flex-direction: row;
-    align-items: flex-start;
-    direction: rtl;
+html[dir="rtl"] .drawer-overlay {
+  flex-direction: row;
+  align-items: flex-start;
+  direction: rtl;
+}
 
-    &.left {
-      flex-direction: row-reverse;
-    }
-  }
+html[dir="rtl"] .drawer-overlay.left {
+  flex-direction: row-reverse;
 }
 </style>
