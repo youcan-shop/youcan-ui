faf677995708560094ee54b5d84561c0ba61afb6
mosarabi <149577299+mosarabi@users.noreply.github.com>
2024-03-12 12:03:18 +0000

    SettingsNav > Improvement

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -1,28 +1,49 @@
 <script setup lang="ts">
 import 'uno.css';
 import '../assets/main.css';
-import { Badge } from '~/components';
-import type { BadgeState } from '~/types';
-const badgeLabel = 'Badge';
-const labelStates: BadgeState[] = ['danger', 'info', 'neutral', 'success', 'warning'];
+import { ref } from 'vue';
+import { SettingsNav, SettingsNavItem } from '~/components';
+
+const SETTINGS = [
+  { label: 'General', icon: 'i-youcan-gear' },
+  { label: 'Account', icon: 'i-youcan-user' },
+  { label: 'Privacy', icon: 'i-youcan-lock' },
+  { label: 'Appearance', icon: 'i-youcan-palette' },
+  { label: 'Subscriptions', icon: 'i-youcan-receipt' },
+];
+const activeTab = ref(SETTINGS[0].label);
+
+const handleClick = (tab: string) => {
+  activeTab.value = tab;
+};
 </script>
 
 <template>
   <div class="container">
-    <Badge v-for="state in labelStates" :key="state" :state="state" :size="24">
-      {{ badgeLabel }}
-    </Badge>
+    <SettingsNav>
+      <template #header>
+        <p>Settings</p>
+      </template>
+      <template #items>
+        <SettingsNavItem
+          v-for="item in SETTINGS"
+          :key="item.label"
+          :label="item.label"
+          :icon="item.icon"
+          :active="activeTab === item.label"
+          @click="() => handleClick(item.label)"
+        />
+      </template>
+    </SettingsNav>
   </div>
 </template>
 
 <style scoped>
 .container {
-  display: flex;
-  gap: 10px;
-  align-items: center;
-  justify-content: center;
-  width: 40vw;
-  height: 40vh;
-  margin: 10%;
+  min-width: 100%;
+}
+
+.settings-nav {
+  color: black;
 }
 </style>
