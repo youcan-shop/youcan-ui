2b6b5fe3477b945b3b6cf79e96672d5403656171
mosarabi <149577299+mosarabi@users.noreply.github.com>
2024-03-12 11:15:05 +0000

    replace scss by css

--- packages/vue3/src/components/Tooltip/Tooltip.vue
+++ packages/vue3/src/components/Tooltip/Tooltip.vue
@@ -47,38 +47,36 @@ onUnmounted(() => {
   <slot v-else />
 </template>
 
-<style scoped lang="scss">
+<style scoped>
 .tooltip {
   display: inline-block;
   position: relative;
+}
 
-  .tooltip-trigger {
-    visibility: hidden;
-    position: fixed;
-    z-index: 9999999999;
-    width: max-content;
-    max-width: 200px;
-    padding: 8px 12px;
-    transition: opacity 0.3s ease-in-out;
-    transition-delay: 0.1s;
-    border-radius: 4px;
-    opacity: 0;
-    background-color: var(--base-black);
-    color: var(--base-white);
-    font: var(--text-xs-regular);
+.tooltip .tooltip-trigger {
+  visibility: hidden;
+  position: fixed;
+  z-index: 9999999999;
+  width: max-content;
+  max-width: 200px;
+  padding: 8px 12px;
+  transition: opacity 0.3s ease-in-out;
+  transition-delay: 0.1s;
+  border-radius: 4px;
+  opacity: 0;
+  background-color: var(--base-black);
+  color: var(--base-white);
+  font: var(--text-xs-regular);
+}
 
-    &-hide {
-      visibility: hidden !important;
-      opacity: 0 !important;
-    }
-  }
+.tooltip .tooltip-trigger-hide {
+  visibility: hidden !important;
+  opacity: 0 !important;
+}
 
-  &:hover {
-    .tooltip-trigger {
-      visibility: visible;
-      transition-delay: 0.2s;
-      opacity: 1;
-    }
-  }
+.tooltip:hover .tooltip-trigger {
+  visibility: visible;
+  transition-delay: 0.2s;
+  opacity: 1;
 }
 </style>
