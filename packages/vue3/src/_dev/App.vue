72a422d6a19b4c7f50aedfb523f3504ac769f481
mosarabi <149577299+mosarabi@users.noreply.github.com>
2024-03-13 10:22:35 +0000

    UI 296: Drawer > Improvement (#295)

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -1,35 +1,99 @@
 <script setup lang="ts">
 import 'uno.css';
 import '../assets/main.css';
-import { Thumbnail } from '~/components';
-
-const imageSrc = 'https://i.imgur.com/YcP0tik.jpeg';
-const imageAlt = 'Mr. Shark';
+import { ref } from 'vue';
+import { Drawer, PrimaryButton } from '~/components';
+const showDrawer = ref(false);
 </script>
 
 <template>
+  <Drawer v-model:visible="showDrawer">
+    <div class="drawer-content">
+      <img class="banner" src="https://developer.youcan.shop/banner.webp" alt="">
+      <div class="content">
+        <h1 class="title">
+          Get started with YouCan UI
+        </h1>
+        <p class="description">
+          Create your app using our UI library to insure seemless integration and user experience
+        </p>
+        <ul class="features">
+          <li>👔 Tailored for you</li>
+          <li>🏢 Internal and External Use</li>
+          <li>⚙️ Vue.js and CSS</li>
+        </ul>
+      </div>
+      <PrimaryButton class="cta" @click="showDrawer = false">
+        <span>Get Started</span>
+      </PrimaryButton>
+    </div>
+  </Drawer>
   <div class="container">
-    <Thumbnail
-      :src="imageSrc"
-      :alt="imageAlt"
-      size="large"
-    />
-    <Thumbnail
-      :src="imageSrc"
-      :alt="imageAlt"
-      size="small"
-    />
+    <PrimaryButton @click="showDrawer = true">
+      <span>Open Drawer</span>
+    </PrimaryButton>
   </div>
 </template>
 
 <style scoped>
 .container {
   display: flex;
-  gap: 10px;
   align-items: center;
   justify-content: center;
-  width: 40vw;
-  height: 40vh;
-  margin: 10%;
+  min-width: 50%;
+  padding: 16px;
+}
+
+.drawer-content {
+  display: flex;
+  flex-direction: column;
+  max-width: 992px;
+  height: 100%;
+}
+
+.drawer-content .banner {
+  width: 100%;
+  height: 60%;
+  border: 1px solid var(--gray-200);
+  border-radius: 12px;
+  object-fit: cover;
+}
+
+.drawer-content .content {
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.content .title {
+  text-align: center;
+}
+
+.content .description {
+  margin: 0;
+  color: var(--gray-500);
+  text-align: center;
+}
+
+.content .features {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  margin-top: 50px;
+  list-style: none;
+  gap: 16px;
+}
+
+.content .features li {
+  margin: 0;
+  padding: 16px;
+  border: 1px solid var(--gray-200);
+  border-radius: 8px;
+}
+
+.drawer-content .cta {
+  width: 100%;
+  min-height: 55px;
 }
 </style>
