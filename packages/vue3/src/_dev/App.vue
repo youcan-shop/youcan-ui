a7a8b1248b48a59711ca46981e2d48a6e5a029f3
mosarabi <149577299+mosarabi@users.noreply.github.com>
2024-03-13 11:21:05 +0000

    Remove SCSS

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -2,71 +2,41 @@
 import 'uno.css';
 import '../assets/main.css';
 import { ref } from 'vue';
-import { PrimaryButton, ResourcePicker } from '~/components';
-import type { Resource } from '~/types';
-
-const MOCK_RESOURCES: Resource[] = [
-  {
-    id: 1,
-    thumbnailUrl: '',
-    name: 'Apple MacBook Pro',
-    price: '$2,499.00',
-    stock: 7,
-    isChecked: false,
-    variants: [
-      {
-        id: 33,
-        thumbnailUrl: '',
-        name: 'Apple MacBook Pro 16 M3 Max',
-        price: '$3,499.00',
-        stock: 3,
-        isChecked: false,
-      },
-      {
-        id: 21,
-        thumbnailUrl: '',
-        name: 'Apple MacBook Pro 14 M3 Pro',
-        price: '$2,499.00',
-        stock: 4,
-        isChecked: false,
-      },
-    ],
-  },
-  {
-    id: 2,
-    thumbnailUrl: '',
-    name: 'Apple iMac',
-    price: '$1,499.00',
-    stock: 2,
-    isChecked: false,
-  },
-];
-
-const showPicker = ref(false);
-const selectedResources = ref<Resource[]>([]);
-const resources = ref<Resource[]>(MOCK_RESOURCES);
-
-const onConfirm = (resources: Resource[]) => {
-  selectedResources.value = resources;
-  showPicker.value = false;
-};
+import { Popover, PrimaryButton, TertiaryButton } from '~/components';
+const show = ref(false);
 </script>
 
 <template>
-  <div className="container">
-    <ResourcePicker
-      v-model:visible="showPicker"
-      :resources="resources"
-      stock-label="in stock"
-      :is-loading="false"
-      @confirm="onConfirm"
-    />
-    <PrimaryButton @click="showPicker = true;">
-      <span>Open Picker</span>
-    </PrimaryButton>
-    <div v-if="selectedResources.length > 0" class="selection">
-      <pre>{{ selectedResources }}</pre>
-    </div>
+  <div class="container">
+    <Popover
+      v-model:show="show"
+      position="top"
+      object-fit="cover"
+      class="popover"
+      @click-outside="show = false"
+    >
+      <template #title>
+        Introducing Themes!
+      </template>
+      <template #description>
+        <p class="mr-0">
+          Introducing themes, a new way to make your store stand out
+        </p>
+      </template>
+      <template #footer>
+        <div class="actions">
+          <PrimaryButton @click="show = false">
+            Get started
+          </PrimaryButton>
+          <TertiaryButton @click="show = false">
+            Later
+          </TertiaryButton>
+        </div>
+      </template>
+      <PrimaryButton @click="show = !show;">
+        <span>Show Popover</span>
+      </PrimaryButton>
+    </Popover>
   </div>
 </template>
 
@@ -76,18 +46,7 @@ const onConfirm = (resources: Resource[]) => {
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  max-width: 300px;
-  gap: 16px;
-}
-
-.selection {
-  padding: 16px;
-  border: 1px solid var(--gray-200);
-  border-radius: 8px;
-  background-color: var(--vp-c-bg);
-}
-
-:is(.dark) .selection {
-  border-color: var(--gray-700);
+  width: 100vw;
+  height: 100vh;
 }
 </style>
--- packages/vue3/src/components/Popover/Popover.vue
+++ packages/vue3/src/components/Popover/Popover.vue
@@ -10,8 +10,8 @@ const props = withDefaults(defineProps<PopoverProps>(), {
 const emit = defineEmits(['update:show']);
 
 const popover = ref();
-const element = ref();
-const popoverTrigger = ref();
+const trigger = ref();
+const triggeredElement = ref();
 const top = ref('0px');
 const left = ref('0px');
 const currentPosition = ref('');
@@ -19,8 +19,8 @@ const currentPosition = ref('');
 const slots = useSlots();
 
 const setPopoverPosition = () => {
-  if (popoverTrigger.value && element.value) {
-    const position = setPosition(popoverTrigger.value, element.value, props.position, 22);
+  if (triggeredElement.value && trigger.value) {
+    const position = setPosition(triggeredElement.value, trigger.value, props.position, 22);
     top.value = `${position.top}px`;
     left.value = `${position.left}px`;
     currentPosition.value = position.currentPosition;
@@ -49,7 +49,7 @@ onUnmounted(() => {
 <template>
   <div ref="popover" class="popover">
     <Transition name="fade">
-      <div v-if="show" ref="popoverTrigger" class="popover-trigger" :class="[currentPosition, { 'has-footer': slots.footer }]">
+      <div v-if="show" ref="triggeredElement" class="triggered-element" :class="[currentPosition, { 'has-footer': slots.footer }]">
         <div class="content">
           <div v-if="imgSrc" class="image">
             <img :src="imgSrc">
@@ -66,142 +66,127 @@ onUnmounted(() => {
         </div>
       </div>
     </Transition>
-    <div ref="element" @click="setPopoverPosition();">
+    <div ref="trigger" @click="setPopoverPosition();">
       <slot />
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
-$margin: 24px;
-$caret-size: 22px;
-$caret-border: 1px solid var(--gray-300);
-
 .popover {
+  --caret-size: 22px;
+  --caret-border: 1px solid var(--gray-300);
+
   position: relative;
+}
 
-  &-trigger {
-    display: flex;
-    position: fixed;
-    z-index: 9999999999;
-    top: v-bind(top);
-    left: v-bind(left);
-    flex-direction: column;
-    width: 400px;
-    max-width: calc(100vw - 40px);
-    min-height: 70px;
-    border: 1px solid var(--gray-200);
-    border-radius: 8px;
-    background-color: var(--base-white);
-    box-shadow: var(--shadow-xl-gray);
+.triggered-element {
+  display: flex;
+  position: fixed;
+  z-index: 9999999999;
+  top: v-bind(top);
+  left: v-bind(left);
+  flex-direction: column;
+  width: 400px;
+  max-width: calc(100vw - 40px);
+  min-height: 70px;
+  border: 1px solid var(--gray-200);
+  border-radius: 8px;
+  background-color: var(--base-white);
+  box-shadow: var(--shadow-xl-gray);
+}
 
-    &::after {
-      content: "";
-      position: absolute;
-      width: $caret-size;
-      height: $caret-size;
-      border-top: $caret-border;
-      border-right: $caret-border;
-      border-top-right-radius: 2px;
-      background-color: var(--base-white);
-    }
+.triggered-element::after {
+  content: "";
+  position: absolute;
+  width: var(--caret-size);
+  height: var(--caret-size);
+  border-top: var(--caret-border);
+  border-right: var(--caret-border);
+  border-top-right-radius: 2px;
+  background-color: var(--base-white);
+}
 
-    > * {
-      box-sizing: border-box;
-      width: 100%;
-    }
+.triggered-element > * {
+  box-sizing: border-box;
+  width: 100%;
+}
 
-    .content {
-      display: flex;
-      flex-direction: column;
-      row-gap: 6px;
-      padding: 24px;
-      font: var(--text-sm-regular);
+.triggered-element .content {
+  display: flex;
+  flex-direction: column;
+  row-gap: 6px;
+  padding: 24px;
+  font: var(--text-sm-regular);
+}
 
-      .image {
-        width: 100%;
-        height: 220px;
-        margin-bottom: 14px;
-        overflow: hidden;
-        border-radius: 8px;
+.triggered-element .content .image {
+  width: 100%;
+  height: 220px;
+  margin-bottom: 14px;
+  overflow: hidden;
+  border-radius: 8px;
+}
 
-        img {
-          width: 100%;
-          height: 100%;
-          object-fit: v-bind(objectFit);
-        }
-      }
-    }
+.triggered-element .content .image img {
+  width: 100%;
+  height: 100%;
+  object-fit: v-bind(objectFit);
+}
 
-    .title {
-      color: var(--gray-900);
-      font: var(--text-md-bold);
-    }
+.triggered-element .title {
+  color: var(--gray-900);
+  font: var(--text-md-bold);
+}
 
-    .description {
-      color: var(--gray-600);
-    }
+.triggered-element .description {
+  color: var(--gray-600);
+}
 
-    .footer {
-      display: flex;
-      flex-direction: row-reverse;
-      align-items: center;
-      justify-content: space-between;
-      padding: 18px 24px;
-      column-gap: 20px;
-      border-top: 1px solid var(--gray-100);
-      border-radius: 0 0 8px 8px;
-      background-color: var(--gray-50);
-    }
+.triggered-element .footer {
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: center;
+  justify-content: space-between;
+  padding: 18px 24px;
+  column-gap: 20px;
+  border-top: 1px solid var(--gray-100);
+  border-radius: 0 0 8px 8px;
+  background-color: var(--gray-50);
+}
 
-    &.left,
-    &.right {
-      &::after {
-        top: calc(50% - calc($caret-size / 2));
-      }
-    }
+.triggered-element.left::after,
+.triggered-element.right::after {
+  top: calc(50% - var(--caret-size) / 2);
+}
 
-    &.left {
-      &::after {
-        left: calc(100% - calc($caret-size / 2));
-        transform: rotate(45deg);
-      }
-    }
+.triggered-element.left::after {
+  left: calc(100% - var(--caret-size) / 2);
+  transform: rotate(45deg);
+}
 
-    &.right {
-      &::after {
-        right: calc(100% - calc($caret-size / 2));
-        transform: rotate(-135deg);
-      }
-    }
+.triggered-element.right::after {
+  right: calc(100% - var(--caret-size) / 2);
+  transform: rotate(-135deg);
+}
 
-    &.top,
-    &.bottom {
-      &::after {
-        left: calc(50% - calc($caret-size / 2));
-      }
-    }
+.triggered-element.top::after,
+.triggered-element.bottom::after {
+  left: calc(50% - var(--caret-size) / 2);
+}
 
-    &.top {
-      &::after {
-        top: calc(100% - calc($caret-size / 2));
-        transform: rotate(135deg);
-      }
+.triggered-element.top::after {
+  top: calc(100% - var(--caret-size) / 2);
+  transform: rotate(135deg);
+}
 
-      &.has-footer {
-        &::after {
-          background-color: var(--gray-50);
-        }
-      }
-    }
+.triggered-element.top.has-footer::after {
+  background-color: var(--gray-50);
+}
 
-    &.bottom {
-      &::after {
-        bottom: calc(100% - calc($caret-size / 2));
-        transform: rotate(-45deg);
-      }
-    }
-  }
+.triggered-element.bottom::after {
+  bottom: calc(100% - var(--caret-size) / 2);
+  transform: rotate(-45deg);
 }
 
 .fade-enter-active {
