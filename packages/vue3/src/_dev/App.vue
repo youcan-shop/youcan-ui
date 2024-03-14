b6668bbeac8762650f3c6272fb6fe98c14dd7df9
mosarabi <149577299+mosarabi@users.noreply.github.com>
2024-03-14 12:03:48 +0000

    UI-251: Checkbox > Improvement (#302)

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -2,52 +2,25 @@
 import 'uno.css';
 import '../assets/main.css';
 import { ref } from 'vue';
-import { Slider } from '~/components';
-import type { RangeValue } from '~/types';
-
-const value = ref(2000);
-const rangeValue = ref<RangeValue>({ min: 200, max: 500 });
+import { Checkbox } from '~/components';
+const isChecked = ref(false);
 </script>
 
 <template>
-  <div class="container">
-    <div>
-      <Slider
-        v-model="rangeValue"
-        suffix=""
-        :max="2000"
-        :min="200"
-        type="range"
-      />
-      <div>min : {{ rangeValue.min }}  ==> max: {{ rangeValue.max }}</div>
-      <Slider
-        v-model="value"
-        suffix=""
-        :max="2000"
-        :min="200"
-      />
-      <div>{{ value }}</div>
-    </div>
+  <div class="form-control">
+    <Checkbox v-model="isChecked" indeterminate="true">
+      <template #label>
+        Accept terms and conditions
+      </template>
+    </Checkbox>
   </div>
 </template>
 
 <style scoped>
-.container {
+.form-control {
   display: flex;
   align-items: center;
   justify-content: center;
-  width: 100vw;
-  height: 100vh;
-}
-
-.container > div {
-  display: flex;
-  flex-direction: column;
-  width: 450px;
-  gap: 30px;
-}
-
-.container > div > div {
-  font: var(--text-sm-medium);
+  gap: 12px;
 }
 </style>
