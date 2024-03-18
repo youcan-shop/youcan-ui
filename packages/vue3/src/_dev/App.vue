8b2bc9ad206a464c62741be447f1f3e6418163b2
GhitaNektt <152861043+GhitaNektt@users.noreply.github.com>
2024-03-18 10:45:18 +0000

    UI-243 : Added rich text component with quill (#285)
    Co-authored-by: Ghita Nekt <ghitanekt@admins-MacBook-Pro.local>

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -2,59 +2,20 @@
 import 'uno.css';
 import '../assets/main.css';
 import { ref } from 'vue';
-import { Popover, PrimaryButton, TertiaryButton } from '~/components';
-const show = ref(false);
+import { RichText } from '~/components';
+
+const content = ref('');
 </script>
 
 <template>
   <div class="container">
-    <Popover
-      v-model:show="show"
-      position="right"
-      object-fit="cover"
-      class="popover"
-      @click-outside="show = false"
-    >
-      <template #title>
-        Introducing Themes!
-      </template>
-      <template #description>
-        <p class="mr-0">
-          Introducing themes, a new way to make your store stand out
-        </p>
-      </template>
-      <template #footer>
-        <div class="actions">
-          <PrimaryButton @click="show = false">
-            Get started
-          </PrimaryButton>
-          <TertiaryButton @click="show = false">
-            Later
-          </TertiaryButton>
-        </div>
-      </template>
-      <PrimaryButton @click="show = !show;">
-        <span>Show Popover</span>
-      </PrimaryButton>
-    </Popover>
+    <RichText v-model="content" />
+    <p>{{ content }}</p>
   </div>
 </template>
 
 <style scoped>
 .container {
-  display: flex;
-  box-sizing: border-box;
-  align-items: center;
-  justify-content: center;
-  width: 100vw;
-  height: 100vh;
-  padding: 20px;
-}
-
-.actions {
-  display: flex;
-  flex-direction: row-reverse;
-  justify-content: flex-end;
-  column-gap: 10px;
+  margin: 30px;
 }
 </style>
