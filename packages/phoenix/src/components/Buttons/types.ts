a9ff98a62f14a8790805459dc8c03918c1cab22b
Ghita Nekt <ghitanekt@admins-MacBook-Pro.local>
2024-04-17 11:18:21 +0100

    removed unused type

--- packages/phoenix/src/components/Buttons/types.ts
+++ packages/phoenix/src/components/Buttons/types.ts
@@ -1,7 +1,5 @@
 export type ButtonSize = '2xl' | 'xl' | 'lg' | 'md' | 'sm' | 'xs' | 'xxs';
 
-export type ButtonIconPosition = 'left' | 'right' | 'only';
-
 export type ButtonVariants = 'primary' | 'secondary' | 'tertiary' | 'destructive';
 export interface ButtonProps {
   variant?: ButtonVariants
