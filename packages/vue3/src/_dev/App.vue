f83cc913f777026480a348dbf9ee7b1f864ed657
Ghita Nekt <ghitanekt@admins-MacBook-Pro.local>
2024-04-18 15:21:35 +0100

    updated app.vue for review

--- packages/vue3/src/_dev/App.vue
+++ packages/vue3/src/_dev/App.vue
@@ -2,40 +2,14 @@
 import 'uno.css';
 import '../assets/main.css';
 import { ref } from 'vue';
-import { Badge, Divider, DropdownMenu, DropdownMenuItem } from '~/components';
+import { RichText } from '~/components';
 
-const imgSrc = ref('https://i.imgur.com/61WTxIG.jpg');
+const content = ref();
 </script>
 
 <template>
   <div class="container">
-    <DropdownMenu position="bottom">
-      <div class="avatar">
-        <img :src="imgSrc" alt="avatar" srcset="">
-      </div>
-      <template #MenuItems>
-        <DropdownMenuItem label="Account" />
-        <Divider thickness="light" />
-        <DropdownMenuItem label="With accessory">
-          <template #accessory>
-            <i class="i-youcan:rocket-launch" />
-          </template>
-        </DropdownMenuItem>
-        <DropdownMenuItem label="With Badge">
-          <template #icon>
-            <Badge state="success" :size="20">
-              Badge
-            </Badge>
-          </template>
-        </DropdownMenuItem>
-        <DropdownMenuItem label="Notification" />
-        <DropdownMenuItem label="Logout" class="custom-class">
-          <template #icon>
-            <i class="i-youcan:sign-out" />
-          </template>
-        </DropdownMenuItem>
-      </template>
-    </DropdownMenu>
+    <RichText v-model="content" />
   </div>
 </template>
 
