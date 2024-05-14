1ceaf9bd5f5000f5ced69eb0273da18a9903fdca
mosarabi <149577299+mosarabi@users.noreply.github.com>
2024-05-14 09:31:00 +0100

    added install function

--- packages/phoenix/src/index.ts
+++ packages/phoenix/src/index.ts
@@ -1,1 +1,15 @@
+import type { App } from 'vue';
+import * as components from './components';
+import 'uno.css';
+import './assets/main.css';
+
+function install(app: App) {
+  for (const key in components) {
+    // @ts-expect-error idk man
+    app.component(key, components[key]);
+  }
+}
+
 export * from './components';
+
+export default { install };
