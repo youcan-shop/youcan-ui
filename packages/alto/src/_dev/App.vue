cbb8385a5f6d88abec040832706e2c46eee8badf
GhitaNektt <152861043+GhitaNektt@users.noreply.github.com>
2024-06-12 15:32:25 +0100

    Fixed popover animation delay

--- packages/alto/src/_dev/App.vue
+++ packages/alto/src/_dev/App.vue
@@ -1,40 +1,67 @@
 <script setup lang="ts">
 import 'uno.css';
 import '../assets/main.css';
-import { Divider } from '~/components';
+import { ref } from 'vue';
+import { Button, Popover } from '~/components';
+const show = ref(false);
 </script>
 
 <template>
-  <div class="container-vertical">
-    <Divider orientation="vertical" thickness="regular" />
-    <Divider dashed orientation="vertical" thickness="light" />
-    <Divider dashed orientation="vertical" thickness="regular" />
-    <Divider dashed orientation="vertical" thickness="thick" />
-  </div>
   <div class="container">
-    <Divider thickness="light" />
-    <Divider dashed thickness="light" />
-    <Divider dashed thickness="regular" />
-    <Divider dashed thickness="thick" />
+    <Popover
+      v-model:show="show"
+      position="top"
+      object-fit="cover"
+      class="popover"
+      @click-outside="show = false"
+    >
+      <template #title>
+        Introducing Themes!
+      </template>
+      <template #description>
+        <p class="mr-0">
+          Introducing themes, a new way to make your store stand out
+        </p>
+      </template>
+      <template #footer>
+        <div class="actions">
+          <Button variant="primary" @click="show = false">
+            Get started
+          </Button>
+          <Button variant="tertiary" @click="show = false">
+            Later
+          </Button>
+        </div>
+      </template>
+      <Button variant="primary" @click="show = !show;">
+        <span>Show Popover</span>
+      </Button>
+    </Popover>
   </div>
 </template>
 
 <style scoped>
-.container-vertical {
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  max-width: 500px;
-  margin: auto;
-  padding: 0 40px;
-  gap: 40px;
-}
-
 .container {
   display: flex;
   flex-direction: column;
-  height: 50vh;
-  padding: 40px;
-  gap: 30px;
+  align-items: center;
+  justify-content: center;
+  height: 95vh;
+  padding: 10px;
+}
+
+.mr-0 {
+  margin: 0;
+}
+
+.actions {
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: flex-end;
+  column-gap: 10px;
+}
+
+.popover {
+  z-index: 99;
 }
 </style>
