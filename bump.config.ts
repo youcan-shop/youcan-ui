d0559ae3dc47eaeb669801ab8e9e7eef6099d01d
GhitaNektt <152861043+GhitaNektt@users.noreply.github.com>
2024-06-13 11:44:39 +0100

    Added bumpp config

---
+++ bump.config.ts
@@ -0,0 +1,9 @@
+export default {
+  files: [
+    'package.json',
+    'packages/core/package.json',
+    'packages/icons/package.json',
+    'packages/visuals/package.json',
+    'packages/vue3/package.json',
+  ],
+};
