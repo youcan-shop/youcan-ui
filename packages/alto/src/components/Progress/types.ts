921d5b23f06400d5954268e07bd9cc22605bc477
mosarabi <149577299+mosarabi@users.noreply.github.com>
2024-07-03 17:19:32 +0100

    UI-419: Added story to Progress (#383)

--- packages/alto/src/components/Progress/types.ts
+++ packages/alto/src/components/Progress/types.ts
@@ -2,7 +2,6 @@ type ProgressTypes = 'circle' | 'bar';
 export interface ProgressProps {
   value?: number
   maxValue?: number
-  playable?: boolean
   size?: number
   progressType?: ProgressTypes
 }
