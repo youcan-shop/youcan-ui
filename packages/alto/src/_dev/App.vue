e16db5aff66d08b8d0290d41856bf3b2c0b3931e
mosarabi <149577299+mosarabi@users.noreply.github.com>
2024-07-11 11:00:28 +0100

    Added Accordion component

--- packages/alto/src/_dev/App.vue
+++ packages/alto/src/_dev/App.vue
@@ -1,25 +1,53 @@
 <script setup lang="ts">
 import 'uno.css';
 import '../assets/main.css';
-import { Loader } from '~/components';
+import { ref } from 'vue';
+import { Accordion, AccordionItem } from '~/components';
+
+const show = ref(true);
+const current = ref(-1);
+
+function toggle(index: number) {
+  if (index === current.value) {
+    current.value = -1;
+
+    return;
+  }
+
+  current.value = index;
+}
 </script>
 
 <template>
   <div class="container">
-    <Loader variant="spinner" label="Loading..." />
-    <Loader variant="brand" />
-    <Loader variant="bar" />
+    <Accordion>
+      <AccordionItem :show="show" label="Accordion row collapsed default" @toggle="() => show = !show">
+        Placeholder for accordion text.
+      </AccordionItem>
+      <AccordionItem disabled label="Accordion row collapsed disabled" />
+    </Accordion>
+
+    <Accordion>
+      <AccordionItem
+        v-for="index in 5"
+        :key="index"
+        :show="index === current"
+        :label="`Accordion row ${index}`"
+        @toggle="toggle(index)"
+      >
+        Placeholder for accordion text.
+      </AccordionItem>
+    </Accordion>
   </div>
 </template>
 
 <style scoped>
 .container {
   display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  width: 100vw;
-  height: 100vh;
+  flex-direction: column;
+  width: 500px;
+  margin: 60px auto;
   gap: 80px;
+  row-gap: 40px;
 }
 </style>
