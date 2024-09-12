37038ee6193a945942e577a17856083f9fc85bdb
GhitaNektt <152861043+GhitaNektt@users.noreply.github.com>
2024-09-12 14:17:08 +0100

    extracted badge style

--- packages/alto/src/components/Badge/Badge.vue
+++ packages/alto/src/components/Badge/Badge.vue
@@ -11,53 +11,11 @@ withDefaults(
 </script>
 
 <template>
-  <span class="badge" :class="[`state-${state}`, `size-${size}`]">
+  <span class="yc-badge" :class="[`yc-state-${state}`, `yc-size-${size}`]">
     <slot />
   </span>
 </template>
 
 <style scoped>
-.badge.size-sm {
-  --size: 20px;
-}
-
-.badge.size-md {
-  --size: 24px;
-}
-
-.badge {
-  --background-color: var(--gray-300);
-
-  display: flex;
-  box-sizing: border-box;
-  align-items: center;
-  justify-content: center;
-  width: min-content;
-  height: var(--size);
-  padding: 0 6px;
-  border-radius: 100px;
-  background-color: var(--background-color);
-  color: var(--base-white);
-  font: var(--text-sm-medium);
-}
-
-.badge.state-neutral {
-  --background-color: var(--gray-500);
-}
-
-.badge.state-danger {
-  --background-color: var(--red-500);
-}
-
-.badge.state-info {
-  --background-color: var(--blue-500);
-}
-
-.badge.state-warning {
-  --background-color: var(--yellow-500);
-}
-
-.badge.state-success {
-  --background-color: var(--green-600);
-}
+@import "@youcan/styles/badge";
 </style>
