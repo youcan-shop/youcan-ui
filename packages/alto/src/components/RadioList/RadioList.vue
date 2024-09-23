9e05f9a10b910200d780d4adb4e2d9ef32b95652
mosarabi <149577299+mosarabi@users.noreply.github.com>
2024-09-23 10:12:20 +0100

    Added BEM to style

--- packages/alto/src/components/RadioList/RadioList.vue
+++ packages/alto/src/components/RadioList/RadioList.vue
@@ -14,10 +14,10 @@ const model = computed({
 
 <template>
   <div class="yc-radio-list">
-    <label v-for="option in options" :key="option.label" class="yc-radio-item" :for="option.label">
-      <input :id="option.label" v-model="model" :name="option.label" :value="option.label" type="radio" class="yc-trigger">
-      <span class="yc-name">{{ option.label }}</span>
-      <span class="yc-suffix">{{ option.suffix }}</span>
+    <label v-for="option in options" :key="option.label" class="yc-radio-list-item" :for="option.label">
+      <input :id="option.label" v-model="model" :name="option.label" :value="option.label" type="radio" class="yc-radio-list-item-trigger">
+      <span class="yc-radio-list-item-name">{{ option.label }}</span>
+      <span class="yc-radio-list-item-suffix">{{ option.suffix }}</span>
     </label>
   </div>
 </template>
