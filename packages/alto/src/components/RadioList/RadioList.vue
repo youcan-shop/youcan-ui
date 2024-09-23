af4e678b676c437e89cc6cdf2e6b12bb76511c71
mosarabi <149577299+mosarabi@users.noreply.github.com>
2024-09-23 09:47:08 +0100

    UI-476: Extracted RadioList style component

--- packages/alto/src/components/RadioList/RadioList.vue
+++ packages/alto/src/components/RadioList/RadioList.vue
@@ -13,68 +13,15 @@ const model = computed({
 </script>
 
 <template>
-  <div class="radio-list">
-    <label v-for="option in options" :key="option.label" class="radio-item" :for="option.label">
-      <input :id="option.label" v-model="model" :name="option.label" :value="option.label" type="radio" class="trigger">
-      <span class="name">{{ option.label }}</span>
-      <span class="suffix">{{ option.suffix }}</span>
+  <div class="yc-radio-list">
+    <label v-for="option in options" :key="option.label" class="yc-radio-item" :for="option.label">
+      <input :id="option.label" v-model="model" :name="option.label" :value="option.label" type="radio" class="yc-trigger">
+      <span class="yc-name">{{ option.label }}</span>
+      <span class="yc-suffix">{{ option.suffix }}</span>
     </label>
   </div>
 </template>
 
 <style scoped>
-.radio-list {
-  display: flex;
-  box-sizing: border-box;
-  flex-direction: column;
-  gap: 8px;
-}
-
-.radio-item {
-  display: flex;
-  align-items: center;
-  gap: 8px;
-  padding: 11.5px 16px;
-  border: 1px solid var(--gray-200);
-  border-radius: 8px;
-  box-shadow: var(--shadow-xs-gray);
-  cursor: pointer;
-}
-
-.name,
-.suffix {
-  color: var(--gray-900);
-  font: var(--text-sm-regular);
-}
-
-.name {
-  flex-grow: 1;
-}
-
-.trigger {
-  width: 14px;
-  height: 14px;
-  margin: 0;
-  transition: 75ms all linear;
-  border: 1px solid var(--gray-200);
-  border-radius: 50%;
-  outline: 2px solid var(--base-white);
-  background-color: var(--base-white);
-  appearance: none;
-}
-
-.trigger:hover {
-  border-color: var(--gray-300);
-  background-color: var(--gray-50);
-}
-
-.trigger:active {
-  background-color: var(--gray-100);
-}
-
-.trigger:checked {
-  border: 3px solid var(--base-white);
-  outline-color: var(--brand-500);
-  background-color: var(--brand-500);
-}
+@import "@youcan/styles/radio-list";
 </style>
