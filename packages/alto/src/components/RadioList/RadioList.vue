cbf81d107dadb0fdd852d791d9846d7a192533ce
mosarabi <149577299+mosarabi@users.noreply.github.com>
2024-09-27 15:01:12 +0100

    feat: implemented BEM methodology

--- packages/alto/src/components/RadioList/RadioList.vue
+++ packages/alto/src/components/RadioList/RadioList.vue
@@ -14,10 +14,10 @@ const model = computed({
 
 <template>
   <div class="yc-radio-list">
-    <label v-for="option in options" :key="option.label" class="yc-radio-list-item" :for="option.label">
-      <input :id="option.label" v-model="model" :name="option.label" :value="option.label" type="radio" class="yc-radio-list-item-trigger">
-      <span class="yc-radio-list-item-name">{{ option.label }}</span>
-      <span class="yc-radio-list-item-suffix">{{ option.suffix }}</span>
+    <label v-for="option in options" :key="option.label" class="yc-radio-list__item" :for="option.label">
+      <input :id="option.label" v-model="model" :name="option.label" :value="option.label" type="radio" class="yc-radio-list__item__trigger">
+      <span class="yc-radio-list__item__name">{{ option.label }}</span>
+      <span class="yc-radio-list__item__suffix">{{ option.suffix }}</span>
     </label>
   </div>
 </template>
