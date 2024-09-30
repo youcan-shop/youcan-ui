bffcc9b8ded4f3f596c2f69033758cd6a12c6e9a
mosarabi <149577299+mosarabi@users.noreply.github.com>
2024-09-30 12:06:40 +0100

    UI-472: Extracted Divider style component (#411)

--- packages/alto/src/components/Divider/Divider.vue
+++ packages/alto/src/components/Divider/Divider.vue
@@ -12,59 +12,9 @@ withDefaults(
 </script>
 
 <template>
-  <div :class="[orientation, thickness, { dashed }]" />
+  <div :class="[`yc-divider--${orientation}`, `yc-divider--${thickness}`, { 'yc-divider--dashed': dashed }]" />
 </template>
 
 <style scoped>
-.horizontal {
-  width: 100%;
-  max-height: 0;
-  margin-top: 16px;
-  margin-bottom: 16px;
-  border-style: solid;
-  border-color: var(--gray-200);
-}
-
-.vertical {
-  max-width: 0;
-  height: 100%;
-  margin-right: 16px;
-  margin-left: 16px;
-  border-style: solid;
-  border-color: var(--gray-200);
-}
-
-.light {
-  border-width: 0.5px;
-}
-
-.regular {
-  border-width: 1px;
-}
-
-.thick {
-  border-width: 4px;
-}
-
-.vertical.dashed {
-  border-image:
-    repeating-linear-gradient(
-      to bottom,
-      var(--gray-200) 0,
-      var(--gray-200) 10px, /* Length of dash */
-      transparent 5px,
-      transparent 16px /* Length of gap */
-    ) 1;
-}
-
-.horizontal.dashed {
-  border-image:
-    repeating-linear-gradient(
-      to right,
-      var(--gray-200) 0,
-      var(--gray-200) 10px, /* Length of dash */
-      transparent 5px,
-      transparent 16px /* Length of gap */
-    ) 1;
-}
+@import "@youcan/styles/divider";
 </style>
