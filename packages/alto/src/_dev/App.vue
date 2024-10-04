c6da1f1b5783db7e36bef51005831a4ff7731954
GhitaNektt <152861043+GhitaNektt@users.noreply.github.com>
2024-10-04 16:52:15 +0100

    UI-484 : Extracted Input component styles (#418)

--- packages/alto/src/_dev/App.vue
+++ packages/alto/src/_dev/App.vue
@@ -1,10 +1,44 @@
 <script setup lang="ts">
 import 'uno.css';
 import '../assets/main.css';
-import { Thumbnail } from '~/components';
+import { ref } from 'vue';
+import { Input, InputGroup } from '~/components';
+
+const username = ref('');
+const weight = ref('');
 </script>
 
 <template>
-  <Thumbnail src="https://picsum.photos/200/300" alt="Mr. Shark" />
-  <Thumbnail src="https://picsum.photos/200/300" alt="Mr. Shark" size="large" />
+  <div class="container">
+    <Input v-model="username" placeholder="Please enter your username" />
+    <InputGroup label="New username">
+      <template #input>
+        <Input v-model="username" placeholder="Enter username" />
+      </template>
+    </InputGroup>
+    <Input v-model="weight" type="number" placeholder="Enter weight" min="1">
+      <template #suffix>
+        Kg
+      </template>
+    </Input>
+
+    <Input v-model="username" error placeholder="Please enter your username" />
+
+    <InputGroup label="New username" error="The username field is required.">
+      <template #input>
+        <Input v-model="username" error placeholder="Enter username" />
+      </template>
+    </InputGroup>
+  </div>
 </template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  width: 40%;
+  margin: 0 auto;
+  gap: 10px;
+  padding: 10px;
+}
+</style>
